d8900f74778f1c4e06648de61ab63c0e6c3cc4cd
Sacha Greif <SachaG@users.noreply.github.com>
2022-05-27 07:02:20 +0900

    Add surveys to API

--- api/src/resolvers/query.ts
+++ api/src/resolvers/query.ts
@@ -2,9 +2,11 @@ import { SurveyType } from '../types'
 import { getEntities, getEntity } from '../entities'
 import { getLocales, getLocaleObject, getTranslation } from '../i18n'
 import { SurveyConfig } from '../types'
+import surveys from '../surveys'
 
 export default {
     Query: {
+        surveys: () => surveys,
         survey: (parent: any, { survey }: { survey: SurveyType }) => ({
             survey
         }),
@@ -14,12 +16,25 @@ export default {
         }),
         entities: (
             parent: any,
-            { ids, type, tag, tags }: { ids: string[], type: string; tag: string, tags: string[] }
+            { ids, type, tag, tags }: { ids: string[]; type: string; tag: string; tags: string[] }
         ) => getEntities({ ids, type, tag, tags }),
         translation: (parent: any, { key, localeId }: { key: string; localeId: string }) =>
             getTranslation(key, localeId),
-        locale: (parent: any, { localeId, contexts, enableFallbacks }: { localeId: string; contexts: string[], enableFallbacks?: boolean }) =>
-            getLocaleObject(localeId, contexts, enableFallbacks),
-        locales: (parent: any, { contexts, localeIds, enableFallbacks }: { contexts: string[], localeIds: string[], enableFallbacks?: boolean }) => getLocales(contexts, enableFallbacks, localeIds)
+        locale: (
+            parent: any,
+            {
+                localeId,
+                contexts,
+                enableFallbacks
+            }: { localeId: string; contexts: string[]; enableFallbacks?: boolean }
+        ) => getLocaleObject(localeId, contexts, enableFallbacks),
+        locales: (
+            parent: any,
+            {
+                contexts,
+                localeIds,
+                enableFallbacks
+            }: { contexts: string[]; localeIds: string[]; enableFallbacks?: boolean }
+        ) => getLocales(contexts, enableFallbacks, localeIds)
     }
 }
---
+++ api/src/surveys.ts
@@ -0,0 +1,28 @@
+import jsConfig from '../../surveys/stateofjs/config.yml'
+import js2019Config from '../../surveys/stateofjs/2019/config.yml'
+import js2020Config from '../../surveys/stateofjs/2020/config.yml'
+import js2021Config from '../../surveys/stateofjs/2021/config.yml'
+
+import cssConfig from '../../surveys/stateofcss/config.yml'
+import css2020Config from '../../surveys/stateofcss/2020/config.yml'
+import css2021Config from '../../surveys/stateofcss/2021/config.yml'
+
+import graphqlConfig from '../../surveys/stateofgraphql/config.yml'
+import graphql2022Config from '../../surveys/stateofgraphql/2022/config.yml'
+
+const surveys = [
+    {
+        ...jsConfig,
+        editions: [js2019Config, js2020Config, js2021Config]
+    },
+    {
+        ...cssConfig,
+        editions: [css2020Config, css2021Config]
+    },
+    {
+        ...graphqlConfig,
+        editions: [graphql2022Config]
+    }
+]
+
+export default surveys
