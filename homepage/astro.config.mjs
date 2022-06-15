2fc1332386211cd5696eec9873c3e0c9bf32e145
Sacha Greif <SachaG@users.noreply.github.com>
2022-06-15 11:19:30 +0900

    Try fixing redirects

--- homepage/astro.config.mjs
+++ homepage/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
-import netlify from '@astrojs/netlify/functions'
+// import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
