0c40f326a08480729c27a4f4445af20802f6af3b
eric-burel <eb@lbke.fr>
2022-09-06 11:11:33 +0200

    use React jsx for tsup

--- surveyform/tsup.config.ts
+++ surveyform/tsup.config.ts
@@ -16,7 +16,7 @@ const commonConfig = {
   // Skip until .d.ts.map is also supported https://github.com/egoist/tsup/issues/564
   // dts: true,
   sourcemap: true,
-  tsconfig: path.resolve(__dirname, "./tsconfig.json"),
+  tsconfig: path.resolve(__dirname, "./tsconfig.tsup.json"),
   outDir: "scripts/dist",
   name: "",
   platform: "node" as const,
