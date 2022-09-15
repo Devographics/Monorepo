d08712c918a366213e2d2b74e759a1d165e7289e
eric-burel <eb@lbke.fr>
2022-09-15 09:59:35 +0200

    opt out Gatsby from pnpm until we figure Apollo Client correctly

--- surveyform/packages/@vulcanjs/next-apollo/links/error.ts
+++ surveyform/packages/@vulcanjs/next-apollo/links/error.ts
@@ -1,4 +1,4 @@
-import { onError } from "@apollo/client/link/error/index";
+import { onError } from "@apollo/client/link/error";
 import { GraphQLError } from "graphql";
 
 const locationsToStr = (locations: GraphQLError["locations"] = []) =>
