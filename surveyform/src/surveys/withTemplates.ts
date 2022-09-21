8999b8b5d54fecd154e77567e90879c14ac2b63d
Sacha Greif <SachaG@users.noreply.github.com>
2022-09-21 20:55:59 +0900

    Delete withTemplates

--- surveyform/src/surveys/withTemplates.ts
+++
@@ -1,9 +0,0 @@
-import surveys from "~/surveys";
-import { addTemplatesToSurvey } from "~/modules/responses/addTemplateToSurvey";
-
-/**
- * Surveys that can be rendered in React based on their template
- *
- * For non-react code, prefer loading "surveys"
- */
-export const surveysWithTemplates = surveys.map(addTemplatesToSurvey);
