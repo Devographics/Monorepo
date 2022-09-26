c02d4bb7bec84029ed520900bce88ded532d9d0c
Sacha Greif <SachaG@users.noreply.github.com>
2022-09-26 17:18:47 +0900

    core-models fixes

--- shared/core-models/responses/helpers.ts
+++ shared/core-models/responses/helpers.ts
@@ -11,6 +11,6 @@ import pickBy from "lodash/pickBy.js";
 Filter a response object to only keep fields relevant to the survey
 
 */
-export const getResponseData = (response) => {
+export const getResponseData = (response: any) => {
   return pickBy(response, (r, k) => k.includes(response.surveySlug));
 };
