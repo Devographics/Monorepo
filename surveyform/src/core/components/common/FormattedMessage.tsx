b1ca647dc6faa513978b0830f2cd0dc9cc06778c
eric-burel <eb@lbke.fr>
2022-10-05 09:12:54 +0200

    remove DefaultLocaleContext, not used anymore

--- surveyform/src/core/components/common/FormattedMessage.tsx
+++ surveyform/src/core/components/common/FormattedMessage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import sanitizeHtml from "sanitize-html";
 import { useKeydownContext } from "./KeydownContext";
-import { useDefaultLocaleContext } from "./DefaultLocaleContext";
 import { useIntlContext } from "@vulcanjs/react-i18n";
 import { stringsRegistry } from "~/i18n";
 
@@ -27,7 +26,6 @@ export const FormattedMessage = ({
 }) => {
   const intl = useIntlContext();
   const { modKeyDown } = useKeydownContext();
-  const { defaultLocale } = useDefaultLocaleContext();
 
   let message = intl.formatMessage({ id, defaultMessage }, values);
 
@@ -53,24 +51,6 @@ export const FormattedMessage = ({
     props["aria-label"] = id;
     props.title = "Cmd/ctrl-click to add missing translation";
     classNames.push(modKeyDown ? "t-modkeydown" : "t-modkeyup");
-
-    const defaultLocaleMessage = stringsRegistry.getString({
-      id,
-      defaultMessage,
-      values,
-      messages: defaultLocale?.strings || [],
-      locale: defaultLocale?.id || "en-US",
-    });
-
-    if (defaultLocaleMessage) {
-      // a translation was found, but it's a fallback placeholder
-      message = defaultLocaleMessage;
-      classNames.push("t-fallback");
-    } else {
-      // no translation was found
-      message = `[${intl.locale}] ${id}`;
-      classNames.push("t-missing");
-    }
   }
 
   props.className = classNames.join(" ");
