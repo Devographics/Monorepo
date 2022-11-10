2436e942e2e657425ecc0723d8602a69c09040cb
Sacha Greif <SachaG@users.noreply.github.com>
2022-11-10 13:55:24 +0900

    When inserting projects in db, use id as mongo "_id"

--- api/src/projects.ts
+++ api/src/projects.ts
@@ -1,29 +1,27 @@
-import { ProjectMongooseModel } from "@devographics/core-models/server";
-import fetch from "node-fetch";
+import { ProjectMongooseModel } from '@devographics/core-models/server'
+import fetch from 'node-fetch'
 
-const formatId = (id: string) => id?.replaceAll("-", "_");
+const formatId = (id: string) => id?.replaceAll('-', '_')
 
-const idFieldName = "slug";
+const idFieldName = 'slug'
 
 export const initProjects = async (context: any) => {
-  const { db } = context
-  const projectsCollection = db.collection('projects')
+    const { db } = context
+    const projectsCollection = db.collection('projects')
 
-  console.log("// Adding Best of JS projects to DB…");
-  await projectsCollection.deleteMany({});
+    console.log('// Adding Best of JS projects to DB…')
+    await projectsCollection.deleteMany({})
 
+    const response = await fetch('https://bestofjs-static-api.vercel.app/projects-full.json')
+    const BestOfJSData: any = await response.json()
+    const projectsData = BestOfJSData.projects.filter((p: any) => !!p[idFieldName])
 
-  const response = await fetch(
-    "https://bestofjs-static-api.vercel.app/projects-full.json"
-  );
-  const BestOfJSData: any = await response.json();
-  const projectsData = BestOfJSData.projects.filter((p) => !!p[idFieldName]);
-
-  // format all ids (- to _)
-  let data = projectsData.map((project: any) => {
-    return { ...project, id: formatId(project[idFieldName]) };
-  });
-  // TODO: filter out any project that is already in entities
-  await projectsCollection.insertMany(data);
-  console.log(`  -> Inserted ${projectsData.length} projects.`);
-};
+    // format all ids (- to _)
+    let data = projectsData.map((project: any) => {
+        const id = formatId(project[idFieldName])
+        return { ...project, _id: id, id }
+    })
+    // TODO: filter out any project that is already in entities
+    await projectsCollection.insertMany(data)
+    console.log(`  -> Inserted ${projectsData.length} projects.`)
+}
