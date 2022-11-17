7ec201d7014675556c5c61a8c9893d7d0c7d6edd
Sacha Greif <SachaG@users.noreply.github.com>
2022-11-17 17:44:11 +0900

    Do not initialize cache on every startup

--- api/src/init.ts
+++ api/src/init.ts
@@ -3,11 +3,16 @@ import { initSurveys } from './surveys'
 import { initProjects } from './projects'
 import { RequestContext } from './types'
 
-export const init = async ({ context }: { context: RequestContext }) => {
+export const initMemoryCache = async ({ context }: { context: RequestContext }) => {
+    console.log('// Initializing in-memory cache…')
     const entities = await initEntities()
-
     const surveys = await initSurveys()
+    return { entities, surveys }
+}
 
+export const initDbCache = async ({ context, data }: { context: RequestContext; data: any }) => {
+    console.log('// Initializing db cache…')
+    const { surveys, entities } = data
     await cacheSurveysEntities({
         surveys,
         entities,
