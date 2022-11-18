b9de131f4d58ddc972656eb940950c37134468d5
Sacha Greif <SachaG@users.noreply.github.com>
2022-11-18 13:49:34 +0900

    Refactor SurveyAction; show loader until redirect by router.push()

--- surveyform/src/core/components/survey/page/SurveyAction.tsx
+++ surveyform/src/core/components/survey/page/SurveyAction.tsx
@@ -14,149 +14,30 @@ import get from "lodash/get.js";
 import { getSurveyPath } from "~/modules/surveys/getters";
 import isEmpty from "lodash/isEmpty.js";
 import { statuses } from "~/modules/constants";
-
-// for some reason this throws error?
-// TODO: do we need a dynamic require?
-import bowser from "bowser";
-// const bowser = require("bowser"); // CommonJS
-// import { isAdmin as checkIsAdmin } from "@vulcanjs/permissions";
 import { useRouter } from "next/router.js";
 import { useVulcanComponents } from "@vulcanjs/react-ui";
 import { getErrors } from "@vulcanjs/core";
 import { SurveyType } from "@devographics/core-models";
-import gql from "graphql-tag";
-import { getFragmentName } from "@vulcanjs/graphql";
-import { CreateResponseOutputFragment } from "~/modules/responses/fragments";
 import { UserType } from "~/core/models/user";
-import { useQuery } from "@apollo/client";
 import { FormattedMessage } from "~/core/components/common/FormattedMessage";
+import {
+  useSurveyActionParams,
+  useBrowserData,
+  useCurrentUserWithResponses,
+  PrefilledData,
+  useStartSurveyMutation,
+} from "./hooks";
 
 const duplicateResponseErrorId = "error.duplicate_response";
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-const useSurveyActionParams = ():
-  | { paramsReady: false; source: undefined; referrer: undefined }
-  | { paramsReady: true; source?: string; referrer?: string } => {
-  const router = useRouter();
-  const { isReady, isFallback, query } = router;
-  if (!isReady || isFallback)
-    return { paramsReady: false, source: undefined, referrer: undefined };
-  const source = query.source || localStorage.getItem("source");
-  const referrer = query.referrer || localStorage.getItem("referrer");
-  const params = { paramsReady: true } as {
-    paramsReady: true;
-    source?: string;
-    referrer?: string;
-  };
-  if (source) {
-    params.source = source as string;
+const extractError = (rawError: any) => {
+  try {
+    const errorObject = JSON.parse(rawError.message);
+    return errorObject[0]?.data?.errors[0];
+  } catch (error) {
+    return { id: "app.unknown_error" };
   }
-  if (referrer) {
-    try {
-      const { hostname: referrerHostname } = new URL(referrer.toString());
-      const { hostname: currentHostname } = new URL(window.location.href);
-      if (referrerHostname !== currentHostname) {
-        params.referrer = referrer as string;
-      }
-    } catch (error) {
-      // if referrer is not a valid URL, do nothing
-    }
-  }
-  return params;
 };
-
-const useBrowserData = (): BrowserData => {
-  const [browserData, setBrowserData] = useState({});
-  //if (typeof window !== "undefined") {
-  useEffect(() => {
-    const browser = bowser.getParser(window.navigator.userAgent);
-    // TODO: should it need an update?
-    // @ts-expect-error
-    const info = browser.parse().parsedResult;
-    const data = {
-      //...data,
-      common__user_info__device: info.platform.type,
-      common__user_info__browser: info.browser.name,
-      common__user_info__version: info.browser.version,
-      common__user_info__os: info.os.name,
-      common__user_info__referrer: document.referrer,
-    };
-    setBrowserData(data);
-    //if (!data.common__user_info__referrer) {
-    //data.common__user_info__referrer = document.referrer;
-    //}
-  }, []);
-  return browserData;
-};
-
-export const useCurrentUserWithResponses = () => {
-  const {
-    data: currentUserData,
-    loading: loadingCurrentUser,
-    error: any,
-  } = useQuery(
-    gql`
-      query getCurrentUser {
-        currentUser {
-          ...UsersCurrentSurveyAction
-          __typename
-        }
-      }
-
-      fragment UsersCurrentSurveyAction on User {
-        _id
-        username
-        createdAt
-        isAdmin
-        groups
-        responses {
-          _id
-          pagePath
-          surveySlug
-          completion
-          createdAt
-          survey {
-            slug
-            prettySlug
-            name
-            year
-            domain
-            status
-            imageUrl
-            faviconUrl
-            socialImageUrl
-            resultsUrl
-          }
-        }
-        __typename
-      }
-    `
-  );
-  const currentUser = currentUserData?.currentUser;
-  if (!currentUser) return null;
-  return currentUser as UserType;
-};
-
-interface BrowserData {
-  common__user_info__source?: string;
-  common__user_info__referrer?: string;
-  common__user_info__device?: string;
-  common__user_info__browser?: string;
-  common__user_info__version?: string;
-  common__user_info__os?: string;
-}
-interface PrefilledData extends BrowserData {
-  surveySlug?: string;
-  context;
-  email?: string;
-}
-
-
-// const mutationName = "createResponse";
-const mutationName = "startSurvey";
 
 const SurveyAction = ({
   survey,
@@ -166,10 +47,8 @@ const SurveyAction = ({
   currentUser?: UserType;
 }) => {
   //const isAdmin = checkIsAdmin(currentUser);
-  const Components = useVulcanComponents();
+  const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState<Array<any> | undefined>();
-  const { paramsReady, source, referrer } = useSurveyActionParams();
-  const router = useRouter();
   // TODO: what to do if params are not ready?
 
   const { slug, status, context } = survey;
@@ -178,6 +57,83 @@ const SurveyAction = ({
   const currentSurveyResponse = currentUser?.responses?.find(
     (r) => r.surveySlug === slug
   );
+
+  const hasResponse = currentSurveyResponse && !isEmpty(currentSurveyResponse);
+
+  const parsedErrors = errors && errors.map(extractError);
+
+  // hide action button if there is already a duplicate response
+  const hideAction =
+    parsedErrors && parsedErrors.some((e) => e.id === duplicateResponseErrorId);
+
+  const isAvailable =
+    status &&
+    [statuses.preview, statuses.open, statuses.hidden].includes(status);
+
+  const getSurveyAction = () => {
+    if (isAvailable) {
+      // 1. the survey is available to be filled out
+      if (!hasResponse || loading) {
+        // 1a. there is no response, or there is a response but we are currently loading it
+        return (
+          <SurveyStart
+            survey={survey}
+            loading={loading}
+            setLoading={setLoading}
+            currentUser={currentUser}
+            setErrors={setErrors}
+          />
+        );
+      } else {
+        // 1b. there is a response already
+        return (
+          <SurveyLink
+            survey={survey}
+            response={currentSurveyResponse}
+            message="general.continue_survey"
+          />
+        );
+      }
+    } else {
+      // 2. the survey is no longer available
+      return (
+        <SurveyLink
+          survey={survey}
+          message="general.review_survey"
+          {...(hasResponse && { response: currentSurveyResponse })}
+        />
+      );
+    }
+  };
+
+  return (
+    <div className="survey-action">
+      {!hideAction && (
+        <div className="survey-action-inner">{getSurveyAction()}</div>
+      )}
+      {parsedErrors && (
+        <Errors
+          survey={survey}
+          parsedErrors={parsedErrors}
+          currentSurveyResponse={currentSurveyResponse}
+        />
+      )}
+    </div>
+  );
+};
+
+const SurveyStart = ({
+  survey,
+  loading,
+  setLoading,
+  currentUser,
+  setErrors,
+}) => {
+  const { slug, status, context } = survey;
+  const router = useRouter();
+  const { startSurvey, mutationName } = useStartSurveyMutation(survey);
+  const Components = useVulcanComponents();
+  const { paramsReady, source, referrer } = useSurveyActionParams();
 
   // prefilled data
   let data: PrefilledData = {
@@ -198,99 +154,32 @@ const SurveyAction = ({
       browserData?.common__user_info__referrer,
   };
 
-  const hasResponse = currentSurveyResponse && !isEmpty(currentSurveyResponse);
-
-  const extractError = (rawError: any) => {
-    try {
-      const errorObject = JSON.parse(rawError.message);
-      return errorObject[0]?.data?.errors[0];
-    } catch (error) {
-      return { id: "app.unknown_error" };
-    }
-  };
-
-  const surveyCreateResponseOutputFragment =
-    CreateResponseOutputFragment(survey);
-  const mutationButtonProps = {
-    loadingButtonProps: {
-      label: <FormattedMessage id="general.start_survey" />,
-      variant: "primary",
-    },
-    mutation: gql`
-    mutation ${mutationName}($input: CreateResponseInput) {
-      ${mutationName}(input: $input) {
-        ...${getFragmentName(surveyCreateResponseOutputFragment)}
-      }
-    }
-    ${surveyCreateResponseOutputFragment}
-    `,
-    mutationOptions: {
-      /* name: "createResponse",
-      args: { input: "CreateResponseInput" },
-      fragmentName: "CreateResponseOutputFragment",
-      mutationOptions: {*/
-      refetchQueries: ["getCurrentUser"],
-      //},
-    },
-    mutationArguments: { input: { data } },
-    successCallback:
-      (result: // TODO: how to type this better? It's the return of a createMutation, we may have this type already in vulcan?
-      {
-        data: { [mutationName]: { data: { pagePath: string } } };
-      }) => {
+  const loadingButtonProps = {
+    type: "submit",
+    loading,
+    variant: "primary",
+    onClick: async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      try {
+        const result = await startSurvey({
+          variables: { input: { data } },
+        });
+        // no need to stop spinner because it'll disappear when we change page
+        // setLoading(false);
+        console.log(result);
         const pagePath = get(result, `data.${mutationName}.data.pagePath`);
         console.log(`Redirecting to ${pagePath}…`);
         router.push(pagePath);
-      },
-    errorCallback: (error) => {
-      setErrors(getErrors(error));
+      } catch (error) {
+        setErrors(getErrors(error));
+      }
     },
   };
-
-  const parsedErrors = errors && errors.map(extractError);
-  // hide action button if there is already a duplicate response
-  const hideAction =
-    parsedErrors && parsedErrors.some((e) => e.id === duplicateResponseErrorId);
-
   return (
-    <div className="survey-action">
-      {!hideAction && (
-        <div className="survey-action-inner">
-          {status === statuses.preview ||
-          status === statuses.open ||
-          status === statuses.hidden ? (
-            hasResponse ? (
-              <SurveyLink
-                survey={survey}
-                response={currentSurveyResponse}
-                message="general.continue_survey"
-              />
-            ) : (
-              <Components.MutationButton {...mutationButtonProps} />
-            )
-          ) : status === statuses.closed ? (
-            hasResponse ? (
-              <SurveyLink
-                survey={survey}
-                response={currentSurveyResponse}
-                message="general.review_survey"
-              />
-            ) : (
-              <SurveyLink survey={survey} message="general.review_survey" />
-            )
-          ) : null}
-        </div>
-      )}
-      {parsedErrors &&
-        parsedErrors.map((error, i) => (
-          <ErrorItem
-            key={i}
-            error={error}
-            survey={survey}
-            response={currentSurveyResponse}
-          />
-        ))}
-    </div>
+    <Components.LoadingButton {...loadingButtonProps}>
+      <FormattedMessage id="general.start_survey" />
+    </Components.LoadingButton>
   );
 };
 
@@ -323,8 +212,21 @@ const SurveyLink = ({
   );
 };
 
+const Errors = ({ survey, parsedErrors, currentSurveyResponse }) => {
+  return (
+    <>
+      {parsedErrors.map((error, i) => (
+        <ErrorItem
+          key={i}
+          error={error}
+          survey={survey}
+          response={currentSurveyResponse}
+        />
+      ))}
+    </>
+  );
+};
 const ErrorItem = ({ survey, error, response }) => {
-  const Components = useVulcanComponents();
   console.log(error);
   const { id, message, properties } = error;
   return (
