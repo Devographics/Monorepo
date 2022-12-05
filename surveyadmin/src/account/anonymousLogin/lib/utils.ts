33f56e1330d46a920f3ea0d49974e7eccf2476bf
eric-burel <eb@lebrun-burel.com>
2022-12-05 16:04:36 +0100

    remove anonymous login from admin form

--- surveyadmin/src/account/anonymousLogin/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * NOTE: making this a function helps testing,
- * because process may be undefined
- * @returns
- */
-export const isAnonymousAuthEnabled = () =>
-  process.env.NEXT_PUBLIC_ENABLE_ANONYMOUS_AUTH;
