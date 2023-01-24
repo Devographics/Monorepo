ff20fb8becc40a3098857dbcffd9d0de08ef490e
SachaG <358832+SachaG@users.noreply.github.com>
2023-01-24 18:33:33 +0900

    Formatting

--- results/src/core/blocks/filters/helpers.ts
+++ results/src/core/blocks/filters/helpers.ts
@@ -397,13 +397,11 @@ https://www.joshwcomeau.com/react/persisting-react-state-in-localstorage/
  */
 export function useStickyState(defaultValue: any, key: string) {
     const [value, setValue] = useState(() => {
-      const stickyValue = window.localStorage.getItem(key);
-      return stickyValue !== null
-        ? JSON.parse(stickyValue)
-        : defaultValue;
-    });
+        const stickyValue = window.localStorage.getItem(key)
+        return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue
+    })
     useEffect(() => {
-      window.localStorage.setItem(key, JSON.stringify(value));
-    }, [key, value]);
-    return [value, setValue];
-  }
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+    return [value, setValue]
+}
