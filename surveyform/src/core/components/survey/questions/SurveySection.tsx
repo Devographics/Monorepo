e47f59c62acdf4d7e978ee764a09f1c37158ae05
eric-burel <eb@lbke.fr>
2023-01-25 11:06:38 +0100

    fix commit issue

--- surveyform/src/core/components/survey/questions/SurveySection.tsx
+++ surveyform/src/core/components/survey/questions/SurveySection.tsx
@@ -8,14 +8,6 @@ import { useSurvey } from "../SurveyContext/Provider";
 const SurveySection = () => {
   let { responseId, sectionNumber = 1 } = useSurveyResponseSectionParams();
   const survey = useSurvey();
-  // CODE to get the React components too:
-  // import surveys from "~/surveys";
-  //const survey = surveys.find(
-  //  (s) =>
-  //    s.prettySlug === surveyDefinition.slug &&
-  //    s.year === Number(surveyDefinition.year)
-  //);
-  //if (!survey) throw new Error(`Survey with slug ${slug} not found`);
 
   const surveyOutline = survey.outline;
   const sectionIndex = sectionNumber - 1;
