2ef37ac791fc65552c4ee2e012924ce61e127754
eric-burel <eb@lbke.fr>
2023-02-01 07:37:30 +0100

    remove storybook from surveyadmin

--- surveyadmin/.storybook/babel.config.js
+++
@@ -1,7 +0,0 @@
-// @see https://github.com/storybookjs/storybook/issues/12952
-// Needed to get back React classic runtime
-const config = require("../babel.config");
-module.exports = {
-  ...config,
-  presets: ["@babel/preset-env", "@babel/preset-typescript"],
-};
