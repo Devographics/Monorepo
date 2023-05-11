a559c75b85c09eab000e2ba0c71b96b5bcfee2bb
Sacha Greif <git@sachagreif.com>
2023-05-11 15:31:02 +0900

    Add basic form state management

--- surveyform/src/surveys/components/form/FormQuestion.tsx
+++ surveyform/src/surveys/components/form/FormQuestion.tsx
@@ -2,8 +2,24 @@
 import { addComponentToQuestionObject } from "~/responses/customComponents";
 import { parseOptions } from "~/responses/helpers";
 
+const getOnChangeHandler = ({
+  edition,
+  section,
+  question,
+  response,
+  stateStuff,
+}) => {
+  const { updateCurrentValues, setFormState } = stateStuff;
+  return (event) => {
+    const path = question.formPaths.response;
+    const value = event.target.value;
+    updateCurrentValues(path, value);
+  };
+};
+
 export const FormQuestion = (props) => {
-  const { edition, section, response, question } = props;
+  const { edition, section, response, question, stateStuff } = props;
+  const { setFormState } = stateStuff;
   const qWithComponent = addComponentToQuestionObject(question);
   const Component = qWithComponent.input;
   const value = response?.[question.formPaths.response];
@@ -12,6 +28,7 @@ export const FormQuestion = (props) => {
     inputProperties: {
       options,
       value,
+      onChange: getOnChangeHandler(props),
     },
     itemProperties: {
       questionId: question.id,
--- surveyform/src/surveys/components/form/FormSection.tsx
+++ surveyform/src/surveys/components/form/FormSection.tsx
@@ -1,14 +1,44 @@
 "use client";
+import { useState } from "react";
 import FormLayout from "./FormLayout";
 import FormQuestion from "./FormQuestion";
 
+const initFormState = (response) => ({
+  currentValues: {},
+  deletedValues: {},
+});
+
+const mergeWithResponse = (response, currentValues, deletedValues) => {
+  return { ...response, ...currentValues };
+};
+
 export const FormSection = (props) => {
-  const { edition, section, response, sectionNumber } = props;
+  const { edition, section, response: originalResponse, sectionNumber } = props;
+  const [formState, setFormState] = useState(initFormState(originalResponse));
+
+  const updateCurrentValues = (path, value) => {
+    setFormState((currentFormState) => {
+      const { currentValues } = currentFormState;
+      currentValues[path] = value;
+      return { ...currentFormState, currentValues };
+    });
+  };
+
+  const stateStuff = { formState, setFormState, updateCurrentValues };
+
+  const response = mergeWithResponse(
+    originalResponse,
+    formState.currentValues,
+    formState.deletedValues
+  );
+
+  const formProps = { ...props, response, stateStuff };
+
   return (
     <div>
-      <FormLayout {...props}>
+      <FormLayout {...formProps}>
         {section.questions.map((question) => (
-          <FormQuestion key={question.id} {...props} question={question} />
+          <FormQuestion {...formProps} key={question.id} question={question} />
         ))}
       </FormLayout>
     </div>
