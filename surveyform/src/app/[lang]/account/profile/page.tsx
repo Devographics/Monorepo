04f935bc411ad5f81bce28f36701a52ed42ecd2e
eric-burel <eb@lbke.fr>
2023-05-17 12:18:59 +0200

    prepare profile page code

--- surveyform/src/app/[lang]/account/profile/page.tsx
+++ surveyform/src/app/[lang]/account/profile/page.tsx
@@ -4,6 +4,29 @@ import UserResponses from "~/core/components/users/UserResponses";
 import { FormattedMessage } from "~/core/components/common/FormattedMessage";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "~/account/user/api/rsc-fetchers";
+import { getRawResponsesCollection } from "@devographics/mongo";
+import { UserDocument } from "~/core/models/user";
+import { cache } from "react";
+import { ResponseDocument } from "@devographics/core-models";
+
+const getResponses = cache(
+  async ({ currentUser }: { currentUser: UserDocument }) => {
+    const RawResponse = await getRawResponsesCollection<ResponseDocument>();
+    const responsesFromDb = await RawResponse.find({
+      userId: currentUser,
+    }).toArray();
+    // just a defensive permission check
+    responsesFromDb.forEach((r) => {
+      if (r.userId !== currentUser._id) {
+        throw new Error("Got response whose userId doesn't match current user");
+      }
+    });
+    // TODO: restrict fields, double check that the document
+    const responses = responsesFromDb as Array<ResponseDocument>;
+    return [];
+    //return responses;
+  }
+);
 
 const Profile = async () => {
   // TODO: filter out fields the user is not supposed to see
@@ -12,8 +35,7 @@ const Profile = async () => {
     // TODO: use from, require to get current request URL
     return redirect(routes.account.login.href);
   }
-  // TODO: get responses from DB
-  const responses = [];
+  const responses = await getResponses({ currentUser });
   return (
     <div className="contents-narrow account">
       <p>
