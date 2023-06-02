9185e3412770b28d369bb2dfd4aeb3d451effc72
eric-burel <eb@lebrun-burel.com>
2023-06-02 17:19:55 +0200

    reset scroll behaviour, test not ok

--- surveyform/cypress.config.ts
+++ surveyform/cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
-
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
