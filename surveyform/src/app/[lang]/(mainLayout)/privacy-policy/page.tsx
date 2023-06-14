808f5a6b16a5b8363a5e8e8dece9f50d66f4c305
Sacha Greif <git@sachagreif.com>
2023-06-14 10:52:31 +0900

    try force-dynamic fix?

--- surveyform/src/app/[lang]/(mainLayout)/privacy-policy/page.tsx
+++ surveyform/src/app/[lang]/(mainLayout)/privacy-policy/page.tsx
@@ -1,11 +1,13 @@
 import PrivacyPolicy from "./PrivacyPolicy";
 
+export const dynamic = "force-dynamic";
+
 // uncomment to enable static builds
-// import { rscAllLocalesMetadata } from "~/lib/api/rsc-fetchers";
-// export async function generateStaticParams() {
-//   const locales = await rscAllLocalesMetadata();
-//   return locales.map((locale) => ({ lang: locale.id }));
-// }
+import { rscAllLocalesMetadata } from "~/lib/api/rsc-fetchers";
+export async function generateStaticParams() {
+  const locales = await rscAllLocalesMetadata();
+  return locales.map((locale) => ({ lang: locale.id }));
+}
 
 const PrivacyPolicyPage = () => {
   return <PrivacyPolicy />;
