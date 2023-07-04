a1650d02dd84ce456f7461f9693d544a2c76b6e0
Sacha Greif <git@sachagreif.com>
2023-07-04 14:34:34 +0900

    Fix logToFile when not in Next.js environment

--- shared/helpers/conditional_log_to_file.ts
+++ shared/helpers/conditional_log_to_file.ts
@@ -8,7 +8,7 @@ runtime, which doesn't support `fs`
 import type { LogOptions } from './log_to_file'
 
 export const logToFile = async (fileName: string, object: any, options: LogOptions = {}) => {
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
+    if (!process.env.NEXT_RUNTIME || process.env.NEXT_RUNTIME === 'nodejs') {
         const helpers = await import('./log_to_file')
         return helpers.logToFile(fileName, object, options)
     } else {
