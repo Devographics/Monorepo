91a1939214050eb2879ffdb3dac476150c1f01c1
Sacha Greif <git@sachagreif.com>
2023-07-13 18:47:42 +0900

    add cache debugging and flushing features

---
+++ surveyform/src/app/api/debug/flush-cache/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+import { flushCache } from "@devographics/fetch";
+
+export async function GET(req: NextRequest) {
+  const key = req.nextUrl.searchParams.get("key");
+  if (key !== process.env.SECRET_KEY) {
+    return NextResponse.json({ error: "invalid_key" });
+  } else {
+    flushCache();
+    return NextResponse.json({ data: { result: "ok" } });
+  }
+}
