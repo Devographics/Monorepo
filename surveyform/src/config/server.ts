75d3da8e990dea88b3038db0d4b237f4d34f9a2e
Sacha Greif <git@sachagreif.com>
2023-07-14 11:59:15 +0900

    change redis url

--- surveyform/src/config/server.ts
+++ surveyform/src/config/server.ts
@@ -28,7 +28,7 @@ export function serverConfig() {
      */
     translationAPI: process.env.API_URL!,
     mongoUri: process.env.MONGO_URI!,
-    redisUrl: process.env.REDIS_URL!,
+    redisUrl: process.env.REDIS_UPSTASH_URL!,
     // Won't work with upstash, which accepts only HTTP
     // || "redis://localhost:6379",
     redisToken: process.env.REDIS_TOKEN || "",
