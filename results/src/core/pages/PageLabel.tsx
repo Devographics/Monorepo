64026f2b49882798ca0f365b7d04f54dcaccc2bc
eric-burel <eb@lebrun-burel.com>
2023-07-21 16:03:56 +0200

    add readme to surveyform, add types to page

--- results/src/core/pages/PageLabel.tsx
+++ results/src/core/pages/PageLabel.tsx
@@ -1,8 +1,15 @@
 import { getPageLabel } from 'core/helpers/pageHelpers'
 import { useI18n } from 'core/i18n/i18nContext'
+import { PageContextValue } from 'core/types'
 // import { useTools } from 'core/helpers/toolsContext'
 
-const PageLabel = ({ page, includeWebsite }) => {
+const PageLabel = ({
+    page,
+    includeWebsite
+}: {
+    page: PageContextValue
+    includeWebsite?: boolean
+}) => {
     const { getString } = useI18n()
     // const { getToolName } = useTools()
 
