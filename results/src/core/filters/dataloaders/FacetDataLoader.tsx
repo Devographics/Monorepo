c04f3e2f7840e2d23b041aedf1dc01004d2827bf
Sacha Greif <git@sachagreif.com>
2023-08-01 18:44:01 +0900

    Fix facet mode units selection

--- results/src/core/filters/dataloaders/FacetDataLoader.tsx
+++ results/src/core/filters/dataloaders/FacetDataLoader.tsx
@@ -9,6 +9,7 @@ import Loading from 'core/explorer/Loading'
 import styled from 'styled-components'
 import { DataSeries } from '../types'
 import { JSONTrigger } from 'core/blocks/block/BlockData'
+import { BucketUnits } from '@devographics/types'
 
 interface FacetDataLoaderProps extends DynamicDataLoaderProps {
     defaultSeries: DataSeries<AllQuestionData>
@@ -18,7 +19,8 @@ const FacetDataLoader = ({
     defaultSeries,
     block,
     children,
-    chartFilters
+    chartFilters,
+    setUnits
 }: FacetDataLoaderProps) => {
     const pageContext = usePageContext()
     const year = pageContext.currentEdition.year
@@ -40,6 +42,7 @@ const FacetDataLoader = ({
 
             setSeries(seriesData)
             setIsLoading(false)
+            setUnits(BucketUnits.PERCENTAGE_BUCKET)
         }
 
         if (!isEmpty(chartFilters.facet)) {
