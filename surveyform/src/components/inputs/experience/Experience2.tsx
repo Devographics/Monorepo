47d68a8fa36c71eecbbd3deedf092127fa61cc13
Sacha Greif <git@sachagreif.com>
2023-08-08 15:42:16 +0900

    Add second version of follow-up UI for experimentation

---
+++ surveyform/src/components/inputs/experience/Experience2.tsx
@@ -0,0 +1,137 @@
+"use client";
+import { FormItem } from "~/components/form/FormItem";
+import Form from "react-bootstrap/Form";
+
+import { FormattedMessage } from "~/components/common/FormattedMessage";
+import { FormInputProps } from "~/components/form/typings";
+import { FormOption } from "~/components/form/FormOption";
+
+import isEmpty from "lodash/isEmpty.js";
+import { useState } from "react";
+import { DbPathsEnum, OptionMetadata } from "@devographics/types";
+import { getFormPaths } from "@devographics/templates";
+
+import get from "lodash/get.js";
+import { FollowupData, FollowUps } from "./Followup2";
+
+export interface ExperienceProps extends FormInputProps {
+  showDescription: boolean;
+}
+
+export const Experience = (props: ExperienceProps) => {
+  const { value, edition, question, response } = props;
+
+  const { options, entity } = question;
+
+  const formPaths = getFormPaths({ edition, question });
+  // get the paths of the predefined and freeform followup answers
+  // inside the overall response document
+  const predefinedFollowupPath = formPaths[DbPathsEnum.FOLLOWUP_PREDEFINED];
+  const freeformFollowupPath = formPaths[DbPathsEnum.FOLLOWUP_FREEFORM];
+
+  const predefinedFollowupValue =
+    (predefinedFollowupPath && get(response, predefinedFollowupPath)) || [];
+  const freeformFollowupValue =
+    (freeformFollowupPath && get(response, freeformFollowupPath)) || "";
+
+  const hasFollowupData =
+    !isEmpty(predefinedFollowupValue) || !isEmpty(freeformFollowupValue);
+
+  const followupData: FollowupData = {
+    predefinedFollowupPath,
+    freeformFollowupPath,
+    predefinedFollowupValue,
+    freeformFollowupValue,
+  };
+
+  const currentOption = options?.find((o) => o.id === value);
+  return (
+    <FormItem {...props}>
+      {entity?.example && <CodeExample {...entity.example} />}
+      <div className="experience-contents">
+        <div className="experience-options">
+          {options?.map((option, i) => (
+            <ExperienceOption key={i} option={option} i={i} {...props} />
+          ))}
+        </div>
+      </div>
+
+      {currentOption && (
+        <FollowUps
+          option={currentOption}
+          {...props}
+          followupData={followupData}
+        />
+      )}
+    </FormItem>
+  );
+};
+
+const CodeExample = ({ language, code, codeHighlighted }) => {
+  return (
+    <div className="code-example">
+      <h5 className="code-example-heading">
+        <FormattedMessage id="general.code_example" />
+      </h5>
+      <pre>
+        <code dangerouslySetInnerHTML={{ __html: codeHighlighted }}></code>
+      </pre>
+    </div>
+  );
+};
+
+const ExperienceOption = (
+  props: ExperienceProps & { option: OptionMetadata; i: number }
+) => {
+  const {
+    i,
+    edition,
+    response,
+    question,
+    option,
+    path,
+    value,
+    updateCurrentValues,
+    readOnly,
+  } = props;
+  const hasValue = !isEmpty(value);
+  const { followups } = question;
+
+  const isChecked = value === option.id;
+  const checkClass = hasValue
+    ? isChecked
+      ? "form-check-checked"
+      : "form-check-unchecked"
+    : "";
+
+  return (
+    <div className="form-experience-option">
+      <Form.Check
+        key={i}
+        // layout="elementOnly"
+        type="radio"
+      >
+        <Form.Check.Label htmlFor={`${path}.${i}`}>
+          <div className="form-input-wrapper">
+            <Form.Check.Input
+              onChange={(e) => {
+                updateCurrentValues({ [path]: e.target.value });
+              }}
+              type="radio"
+              value={option.id}
+              name={path}
+              id={`${path}.${i}`}
+              // ref={refFunction}
+              checked={isChecked}
+              className={checkClass}
+              disabled={readOnly}
+            />
+          </div>
+          <FormOption {...props} option={option} />
+        </Form.Check.Label>
+      </Form.Check>
+    </div>
+  );
+};
+
+export default Experience;
--- surveyform/src/components/inputs/experience/Feature.tsx
+++ surveyform/src/components/inputs/experience/Feature.tsx
@@ -1,3 +1,15 @@
-import Experience from './Experience'
+import Experience1, { ExperienceProps } from "./Experience";
+import Experience2 from "./Experience2";
 
-export default Experience
+// split component in two while testing
+// to test two possible UIs
+
+const Experience = (props: ExperienceProps) => {
+  if (props.question.template === "featureWithFollowups2") {
+    return <Experience2 {...props} />;
+  } else {
+    return <Experience1 {...props} />;
+  }
+};
+
+export default Experience;
