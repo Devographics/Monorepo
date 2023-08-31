bb0ab8f09a729100575ee90eb25682d0ebc3a9c4
eric-burel <eb@lebrun-burel.com>
2023-08-31 18:20:24 +0200

    TextLink improvement

--- surveyform/src/components/inputs/TextList.tsx
+++ surveyform/src/components/inputs/TextList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React /*, { useState }*/ from "react";
+import React, { useState /*, { useState }*/ } from "react";
 import FormControl from "react-bootstrap/FormControl";
 import { FormInputProps } from "~/components/form/typings";
 import { FormItem } from "~/components/form/FormItem";
@@ -25,47 +25,44 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
 
   const values = value_ || [];
 
-  // TODO: why form update is not immediate as to get a controlled value?
-  // TODO: even with a non immediate value, we should not need that,
-  // just use the HTML input internal state + defaultValue and let it be uncontrolled
+  // TODO: check that the key is correctly set based on "value"
   // @see https://react.dev/learn/you-might-not-need-an-effect#resetting-all-state-when-a-prop-changes
-  // const [localValues, setLocalValues] = useState<Array<string>>(values);
+  const [localValues, setLocalValues] = useState<Array<string>>(values);
 
   // TODO: assess if debouncing is really needed here, onchange is fired only on focus loss
   // (contrary to "oninput" which actually needs debouncing)
   const updateCurrentValuesDebounced = debounce(updateCurrentValues, 500);
 
-  /*
-  const handleChange = (event) => {
-    //setLocalValue(event.target.value);
-    updateCurrentValues({ [path]: event.target.value });
+  const handleChange = (values) => {
+    setLocalValues(values);
+    updateCurrentValuesDebounced({ [path]: values });
   };
-  */
 
   const addItem = (value: string) => {
-    updateCurrentValuesDebounced({
-      [path]: [...values, value],
-    });
+    handleChange([...localValues, value]);
   };
   const removeItem = (idx: number) => {
-    updateCurrentValuesDebounced({
-      // TODO: quick and dirty check if ok
-      [path]: [...values.slice(0, idx), ...values.slice(idx + 1)],
-    });
+    handleChange([...localValues.slice(0, idx), ...localValues.slice(idx + 1)]);
+    // TODO: should we remove the value if the array becomes totally empty?
+    // by setting it to "null"?
   };
   const updateItem = (idx: number, value: string) => {
-    updateCurrentValuesDebounced({
-      [path]: [...values.slice(0, idx), value, ...values.slice(idx + 1)],
-    });
+    handleChange([
+      ...localValues.slice(0, idx),
+      value,
+      ...localValues.slice(idx + 1),
+    ]);
   };
   const handleChangeDebounced = (
     idx: number,
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    event:
+      | React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      | React.FocusEvent<HTMLInputElement | HTMLTextAreaElement> // onBlur
   ) => {
     const value = event.target.value;
     if (!value) {
       removeItem(idx);
-    } else if (idx >= values.length) {
+    } else if (idx >= localValues.length) {
       addItem(value);
     } else {
       updateItem(idx, value);
@@ -74,13 +71,12 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
     updateCurrentValuesDebounced({ [path]: event.target.value });
   };
 
+  const valuesWithAdditionalInput = localValues.length
+    ? [...localValues, ""]
+    : [""];
   return (
     <FormItem {...props}>
-      {[
-        ...values,
-        /** Always keep an additional empty element here */
-        "",
-      ].map((value, idx) => {
+      {valuesWithAdditionalInput.map((value, idx) => {
         return (
           <FormControl
             // TODO: this may mess up rendering, in Vulcan we had specific logic to handle a "visible index"
@@ -90,24 +86,11 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
             defaultValue={value}
             //value={localValue}
             onChange={(evt) => handleChangeDebounced(idx, evt)}
-            // @ts-ignore TODO: TS is not happy with either ChangeEvent or FocusEvent
-            onBlur={handleChangeDebounced}
+            onBlur={(evt) => handleChangeDebounced(idx, evt)}
             disabled={readOnly}
           />
         );
       })}
-      <FormControl
-        // TODO: this may mess up rendering, in Vulcan we had specific logic to handle a "visible index"
-        // need to check what happens when removing an intermediate input, it may mess up the values
-        key={values.length}
-        type="text"
-        defaultValue={""}
-        //value={localValue}
-        onChange={(evt) => handleChangeDebounced(values.length, evt)}
-        // @ts-ignore TODO: TS is not happy with either ChangeEvent or FocusEvent
-        onBlur={handleChangeDebounced}
-        disabled={readOnly}
-      />
     </FormItem>
   );
 };
