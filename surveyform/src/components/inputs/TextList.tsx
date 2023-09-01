5bdff2e201e6fa40fee5ce2ef4c6bbb233e5e05d
eric-burel <eb@lebrun-burel.com>
2023-09-01 11:22:20 +0200

    better textlist

--- surveyform/src/components/inputs/TextList.tsx
+++ surveyform/src/components/inputs/TextList.tsx
@@ -1,9 +1,26 @@
 "use client";
-import React, { useState /*, { useState }*/ } from "react";
+import React, { useEffect, useState /*, { useState }*/ } from "react";
 import FormControl from "react-bootstrap/FormControl";
 import { FormInputProps } from "~/components/form/typings";
 import { FormItem } from "~/components/form/FormItem";
 import debounce from "lodash/debounce.js";
+
+/**
+ * In an array of input with auto-deletion of empty inputs,
+ * each input must be associated with a key to become an entity
+ */
+interface Item {
+  value: string;
+  key: string;
+}
+
+function toStrings(items: Array<Item>): Array<string> {
+  return items.map(({ value }) => value);
+}
+
+function makeItem(value: string): Item {
+  return { value, key: Math.random() + "" };
+}
 
 /**
  * Create additional items as user add values
@@ -27,33 +44,37 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
 
   // TODO: check that the key is correctly set based on "value"
   // @see https://react.dev/learn/you-might-not-need-an-effect#resetting-all-state-when-a-prop-changes
-  const [localValues, setLocalValues] = useState<Array<string>>(values);
+  const [items, setItems] = useState<Array<Item>>(values.map(makeItem));
+  const [itemsWithLast, setItemsWithLast] = useState(items);
+  useEffect(() => {
+    setItemsWithLast(items.length ? [...items, makeItem("")] : [makeItem("")]);
+  }, [items.length]);
 
   // TODO: assess if debouncing is really needed here, onchange is fired only on focus loss
   // (contrary to "oninput" which actually needs debouncing)
   const updateCurrentValuesDebounced = debounce(updateCurrentValues, 500);
 
-  const handleChange = (values) => {
-    setLocalValues(values);
-    updateCurrentValuesDebounced({ [path]: values });
+  const handleChange = (items: Array<Item>) => {
+    setItems(items);
+    updateCurrentValuesDebounced({ [path]: toStrings(items) });
   };
 
-  const addItem = (value: string) => {
-    handleChange([...localValues, value]);
+  const addItem = (item: Item) => {
+    handleChange([...items, item]);
   };
   const removeItem = (idx: number) => {
-    handleChange([...localValues.slice(0, idx), ...localValues.slice(idx + 1)]);
+    handleChange([...items.slice(0, idx), ...items.slice(idx + 1)]);
     // TODO: should we remove the value if the array becomes totally empty?
     // by setting it to "null"?
   };
   const updateItem = (idx: number, value: string) => {
     handleChange([
-      ...localValues.slice(0, idx),
-      value,
-      ...localValues.slice(idx + 1),
+      ...items.slice(0, idx),
+      { value, key: items[idx].key },
+      ...items.slice(idx + 1),
     ]);
   };
-  const handleChangeDebounced = (
+  const handleBlurDebounced = (
     idx: number,
     event:
       | React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -62,31 +83,56 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
     const value = event.target.value;
     if (!value) {
       removeItem(idx);
-    } else if (idx >= localValues.length) {
+    } /*else if (idx >= items.length) {
       addItem(value);
-    } else {
+    }*/ else {
       updateItem(idx, value);
     }
-    //setLocalValue(event.target.value);
-    updateCurrentValuesDebounced({ [path]: event.target.value });
   };
 
-  const valuesWithAdditionalInput = localValues.length
-    ? [...localValues, ""]
-    : [""];
   return (
     <FormItem {...props}>
-      {valuesWithAdditionalInput.map((value, idx) => {
+      {itemsWithLast.map((item, idx) => {
         return (
           <FormControl
             // TODO: this may mess up rendering, in Vulcan we had specific logic to handle a "visible index"
             // need to check what happens when removing an intermediate input, it may mess up the values
-            key={idx}
+            key={item.key}
             type="text"
-            defaultValue={value}
+            defaultValue={item.value}
             //value={localValue}
-            onChange={(evt) => handleChangeDebounced(idx, evt)}
-            onBlur={(evt) => handleChangeDebounced(idx, evt)}
+            //onChange={(evt) => handleChangeDebounced(idx, evt)}
+            onBlur={(evt) => handleBlurDebounced(idx, evt)}
+            onChange={(evt) => {
+              // The last item is displayed but not yet saved in the items list
+              const isLastItem = idx >= items.length;
+              // if we start filling the last item,
+              // actually add it to the items array
+              // /!\ key must stay the same to avoid visual focus jumps
+              const value = evt.target.value;
+              if (isLastItem) {
+                if (value) {
+                  addItem({ value, key: item.key });
+                }
+              }
+              if (!value && idx > 0) {
+                // TODO: focus on last item
+                // (in the prototype this is done via backspace key event,
+                // check if onChange is ok or if we need "onInput" for this case)
+              }
+            }}
+            onKeyUp={(evt) => {
+              if (evt.key === "Enter") {
+                // TODO: focus on the next input (unless we are in the last one)
+                // TODO: we may need to check if current input is still empty or not,
+                // as the focus loss and change event may happen AFTER the keyup
+                // Perhaps we should use "oninput" instead of onchange
+              } else if (evt.key === "ArrowUp") {
+                // TODO: focus on input just above
+              } else if (evt.key === "ArrowDown") {
+                // TODO: focus on input just below
+              }
+            }}
             disabled={readOnly}
           />
         );
