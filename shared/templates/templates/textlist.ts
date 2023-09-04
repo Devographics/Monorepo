58e6e67eee5d3e744158d8793213d47cbc431333
Sacha Greif <git@sachagreif.com>
2023-09-04 18:32:00 +0900

    ⚙️ add "allowMultiple: true"

--- shared/templates/templates/textlist.ts
+++ shared/templates/templates/textlist.ts
@@ -2,7 +2,7 @@ import { text } from './text'
 import { TemplateFunction, QuestionTemplateOutput } from '@devographics/types'
 
 export const textlist: TemplateFunction = options => {
-    const question = { ...options.question, inputComponent: 'textList' }
+    const question = { ...options.question, inputComponent: 'textList', allowMultiple: true }
     const output: QuestionTemplateOutput = text({ ...options, question })
     // if an option is "lost" in the API,
     // check shared/templates/node_modules/@devographics/types/outlines.ts
