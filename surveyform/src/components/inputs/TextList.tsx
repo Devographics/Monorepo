00faa1c480a37a05915b2592d7d3f30734b25d9e
Sacha Greif <git@sachagreif.com>
2023-09-06 20:52:43 +0900

    ✍️ use placeholder string for textList if it exists

--- surveyform/src/components/inputs/TextList.tsx
+++ surveyform/src/components/inputs/TextList.tsx
@@ -5,6 +5,7 @@ import { FormInputProps } from "~/components/form/typings";
 import { FormItem } from "~/components/form/FormItem";
 import debounce from "lodash/debounce.js";
 import { useIntlContext } from "@devographics/react-i18n";
+import { getQuestioni18nIds } from "@devographics/i18n";
 
 /**
  * In an array of input with auto-deletion of empty inputs,
@@ -81,6 +82,7 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
   const {
     path,
     value: value_,
+    section,
     question,
     updateCurrentValues,
     readOnly,
@@ -246,6 +248,7 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
   };
 
   const itemProps = {
+    section,
     question,
     items,
     readOnly,
@@ -273,6 +276,7 @@ export const TextList = (props: FormInputProps<Array<string>>) => {
 };
 
 const TextListItem = ({
+  section,
   question,
   item,
   index,
@@ -281,6 +285,7 @@ const TextListItem = ({
   onChange,
   onKeyDown,
 }: {
+  section: FormInputProps["section"];
   question: FormInputProps["question"];
   item: Item;
   index: number;
@@ -297,6 +302,22 @@ const TextListItem = ({
 }) => {
   const { formatMessage } = useIntlContext();
 
+  const defaultPlaceholder = formatMessage({
+    id: "textlist.placeholder",
+    values: { index: index + 1 },
+  });
+  const i18n = getQuestioni18nIds({ section, question });
+
+  const questionPlaceholder = formatMessage({
+    id: `${i18n.base}.placeholder`,
+    values: { index: index + 1 },
+  });
+
+  const placeholder = questionPlaceholder || defaultPlaceholder;
+
+  console.log(i18n.base);
+  console.log(questionPlaceholder);
+  console.log(defaultPlaceholder);
   return (
     <FormControl
       // id={itemId(item)}
@@ -308,10 +329,7 @@ const TextListItem = ({
       }}
       // TODO: use different templates to simplify?
       as={question.longText ? "textarea" : "input"}
-      placeholder={formatMessage({
-        id: "textlist.placeholder",
-        values: { index: index + 1 },
-      })}
+      placeholder={placeholder}
       defaultValue={item.value}
       //value={localValue}
       //onChange={(evt) => handleChangeDebounced(idx, evt)}
