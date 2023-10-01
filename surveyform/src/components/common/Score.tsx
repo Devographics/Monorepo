5d622cae25f57690ff9699237025272bf4fc8a55
Lea Verou <lea@verou.me>
2023-10-01 10:21:35 -0400

    Drop weird denominators

--- surveyform/src/components/common/Score.tsx
+++ surveyform/src/components/common/Score.tsx
@@ -92,8 +92,8 @@ const Score = ({
       shareUrl: `${questionsUrl}?source=post_survey_share`,
       hashtag,
       // not used by all translations
-      awareness_count: awareness,
-      usage_count: usage,
+      awareness_count: awareness.count,
+      usage_count: usage.count,
     },
   });
 
