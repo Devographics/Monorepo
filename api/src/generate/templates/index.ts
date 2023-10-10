f361feffadd1e334981b329893cf1f40b35f2353
Sacha Greif <git@sachagreif.com>
2023-10-10 16:20:12 +0900

    📡 new years_grouped api question template

--- api/src/generate/templates/index.ts
+++ api/src/generate/templates/index.ts
@@ -14,6 +14,6 @@ export * from './section_tools_ratios'
 export * from './completion_stats'
 export * from './knowledge_score'
 export * from './age_grouped'
-export * from './experience_grouped'
+export * from './years_grouped'
 
 export * from './data_explorer'
