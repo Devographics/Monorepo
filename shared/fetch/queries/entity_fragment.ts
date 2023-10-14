45e4e82c9e91ae63bc9d66e7580f58b3c5e86255
Sacha Greif <git@sachagreif.com>
2023-10-14 20:02:23 +0900

    ⚙️ add exactMatch entity parameter

--- shared/fetch/queries/entity_fragment.ts
+++ shared/fetch/queries/entity_fragment.ts
@@ -47,6 +47,7 @@ ${
     isFull
         ? `
 tags
+exactMatch
 patterns`
         : ''
 }
--- shared/types/entities.ts
+++ shared/types/entities.ts
@@ -4,79 +4,80 @@
  */
 
 export interface EntityResolvedFields {
-  homepage?: Resource;
-  github?: any;
-  npm?: any;
-  w3c?: any;
-  caniuse?: any;
-  mdn?: any;
-  mastodon?: any;
-  twitter?: any;
-  twitch?: any;
-  youtube?: any;
-  company?: Entity;
-  blog?: Resource;
-  rss?: Resource;
+    homepage?: Resource
+    github?: any
+    npm?: any
+    w3c?: any
+    caniuse?: any
+    mdn?: any
+    mastodon?: any
+    twitter?: any
+    twitch?: any
+    youtube?: any
+    company?: Entity
+    blog?: Resource
+    rss?: Resource
 }
 
 export interface Entity extends EntityResolvedFields {
-  id: string;
-  belongsTo?: string;
-  name: string;
-  nameClean?: string;
-  nameHtml?: string;
-  category?: string;
-  description?: string;
-  descriptionClean?: string;
-  descriptionHtml?: string;
-  tags?: string[];
-  patterns?: string[];
-  normalizationOnly?: boolean;
+    id: string
+    belongsTo?: string
+    name: string
+    nameClean?: string
+    nameHtml?: string
+    category?: string
+    description?: string
+    descriptionClean?: string
+    descriptionHtml?: string
+    tags?: string[]
+    patterns?: string[]
+    exactMatch?: boolean
+    normalizationOnly?: boolean
 
-  homepageUrl?: string;
-  blogUrl?: string;
-  rssUrl?: string;
-  mastodonName?: string;
-  twitterName?: string;
-  twitchName?: string;
-  youtubeName?: string;
-  youtubeUrl?: string;
-  companyName?: string;
+    homepageUrl?: string
+    blogUrl?: string
+    rssUrl?: string
+    mastodonName?: string
+    twitterName?: string
+    twitchName?: string
+    youtubeName?: string
+    youtubeUrl?: string
+    companyName?: string
 
-  example?: Example;
-  apiOnly?: boolean;
+    example?: Example
+    apiOnly?: boolean
 
-  resources?: Resource[];
+    resources?: Resource[]
 }
 
 export interface Resource {
-  name?: string;
-  title?: string;
-  url: string;
+    name?: string
+    title?: string
+    url: string
 }
 
 export interface Example {
-  label: string;
-  language: string;
-  code: string;
-  codeHighlighted: string;
+    label: string
+    language: string
+    code: string
+    codeHighlighted: string
 }
 
 export interface MDN {
-  locale: string;
-  url: string;
-  title: string;
-  summary: string;
+    locale: string
+    url: string
+    title: string
+    summary: string
 }
 
 export interface GitHub {
-  id: string;
-  name: string;
-  full_name?: string;
-  description: string;
-  url: string;
-  stars: number;
-  forks?: number;
-  opened_issues?: number;
-  homepage: string;
+    id: string
+    name: string
+    full_name?: string
+    description: string
+    url: string
+    stars: number
+    forks?: number
+    opened_issues?: number
+    homepage: string
 }
