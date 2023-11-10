409cd5aba0e96333f8fa49932b44dbafcb1554f8
Sacha Greif <git@sachagreif.com>
2023-11-11 08:26:17 +0900

    📊 work on making older surveys work again (wip)

--- results/surveys/graphql2022/config/blocks.ts
+++ results/surveys/graphql2022/config/blocks.ts
@@ -1,8 +1,68 @@
-// generic charts
-import HorizontalBarBlock from 'core/blocks/generic/HorizontalBarBlock'
-import VerticalBarBlock from 'core/blocks/generic/VerticalBarBlock'
+/**
+ * Please make sure to comment out unused blocks,
+ * this is gonna help regarding bundle size.
+ */
 
-// other
+/* ---------------------------------------------------------------------- */
+/* ----------------------------  Charts  -------------------------------- */
+/* ---------------------------------------------------------------------- */
+
+// generic charts
+import HorizontalBarBlock from 'core/charts/horizontalBar/index'
+import VerticalBarBlock from 'core/charts/verticalBar'
+// import PeopleBlock from 'core/charts/people'
+// import ToolFeatureExperienceBlock from 'core/charts/toolFeatureExperience'
+
+// features
+import FeaturesOverviewBlock from 'core/charts/featuresOverview'
+import ToolsExperienceRankingSingleYearBlock from 'core/charts/toolsRankingSingleYear'
+import MultiFeaturesExperienceBlock from 'core/charts/multiFeaturesExperience'
+
+// tools
+// import ToolsArrowsBlock from 'core/charts/toolsArrows'
+// import ToolsTierListBlock from 'core/charts/toolsTierList'
+import ToolsScatterplotBlock from 'core/charts/toolsScatterplot'
+
+// import ToolsRatiosLineChartBlock from 'core/charts/toolsRatiosLineChart'
+// import ToolsRatiosRankingBlock from 'core/charts/toolsRatiosRanking'
+// import ToolsSectionStreamsBlock from 'core/charts/toolsSectionStreams'
+import ToolsExperienceMarimekkoBlock from 'core/charts/toolsMarimekko'
+
+// opinions
+import OpinionBlock from 'core/charts/opinion'
+
+// brackets
+import BracketWinsBlock from 'core/charts/brackets/BracketWinsBlock.tsx'
+
+const chartBlocks = {
+    HorizontalBarBlock,
+    VerticalBarBlock,
+    // PeopleBlock,
+
+    // ToolFeatureExperienceBlock,
+    ToolsExperienceRankingSingleYearBlock,
+
+    FeaturesOverviewBlock,
+    MultiFeaturesExperienceBlock,
+
+    // ToolsArrowsBlock,
+    // ToolsTierListBlock,
+    ToolsScatterplotBlock,
+
+    // ToolsRatiosLineChartBlock,
+    // ToolsRatiosRankingBlock,
+    // ToolsSectionStreamsBlock,
+    ToolsExperienceMarimekkoBlock,
+
+    OpinionBlock,
+    BracketWinsBlock
+}
+/* ---------------------------------------------------------------------- */
+/* ----------------------------  Blocks  -------------------------------- */
+/* ---------------------------------------------------------------------- */
+
+import DataExplorerBlock from 'core/explorer/DataExplorerBlock'
+
 import TextBlock from 'core/blocks/other/TextBlock'
 import RecommendedResourcesBlock from 'core/blocks/other/RecommendedResourcesBlock'
 import PageIntroductionBlock from 'core/blocks/other/PageIntroductionBlock'
@@ -12,71 +72,23 @@ import SponsorsBlock from 'core/blocks/other/SponsorsBlock'
 import PartnersBlock from 'core/blocks/other/PartnersBlock'
 import PicksBlock from 'core/blocks/other/PicksBlock'
 import TranslatorsBlock from 'core/blocks/other/TranslatorsBlock'
-// import TshirtBlock from 'core/blocks/other/TshirtBlock'
+import TshirtBlock from 'core/blocks/other/TshirtBlock'
 import AwardsBlock from 'core/blocks/awards/AwardsBlock'
 import ConclusionBlock from 'core/blocks/other/ConclusionBlock'
 import NotFoundBlock from 'core/blocks/other/NotFoundBlock'
 import CreditsBlock from 'core/blocks/other/CreditsBlock'
-// import ReportBlock from 'core/report/ReportBlock'
+
 import HintBlock from 'core/blocks/other/HintBlock'
 import AboutBlock from 'core/blocks/other/AboutBlock'
 import HowToHelpBlock from 'core/blocks/other/HowToHelpBlock'
 import SponsorFinishBlock from 'core/blocks/block/sponsor_chart/SponsorFinishBlock'
-// import LivestreamBlock from 'core/blocks/other/LivestreamBlock'
+import LivestreamBlock from 'core/blocks/other/LivestreamBlock'
 
-// demographics
-import ParticipationByCountryBlock from 'core/blocks/demographics/ParticipationByCountryBlock'
-import GenderBlock from 'core/blocks/demographics/GenderBlock'
-import ByFacetBlock from 'core/blocks/demographics/ByFacetBlock'
-
-// features
-// import FeatureExperienceBlock from 'core/blocks/features/FeatureExperienceBlock'
-import FeaturesOverviewBlock from 'core/blocks/features/FeaturesOverviewBlock'
-import KnowledgeScoreBlock from 'core/blocks/features/KnowledgeScoreBlock'
-import MultiFeaturesExperienceBlock from 'core/blocks/features/MultiFeaturesExperienceBlock'
-
-// tools
-// import ToolHeaderBlock from 'core/blocks/tools/ToolHeaderBlock'
-import ToolExperienceBlock from 'core/blocks/tools/ToolExperienceBlock'
-// import ToolsSectionStreamsBlock from 'core/blocks/tools/ToolsSectionStreamsBlock'
-// import { ToolsExperienceRankingBlock } from 'core/blocks/tools/ToolsExperienceRankingBlock'
-import ToolsExperienceRankingSingleYearBlock from 'core/blocks/tools/ToolsExperienceRankingSingleYearBlock'
-import { ToolsExperienceRadarBlock } from 'core/blocks/tools/ToolsExperienceRadarBlock'
-import { ToolsQuadrantsBlock } from 'core/blocks/tools/ToolsQuadrantsBlock'
-// import { ToolsArrowsBlock } from 'core/blocks/tools/ToolsArrowsBlock'
-import { ToolsExperienceMarimekkoBlock } from 'core/blocks/tools/ToolsExperienceMarimekkoBlock'
-// import { SectionToolsCardinalityByUserBlock } from 'core/blocks/tools/SectionToolsCardinalityByUserBlock'
-import { AllSectionsToolsCardinalityByUserBlock } from 'core/blocks/tools/AllSectionsToolsCardinalityByUserBlock'
-// import { ToolsCityscapeBlock } from 'core/blocks/tools/ToolsCityscapeBlock'
-// import ToolsTierListBlock from 'core/blocks/tools/ToolsTierListBlock'
-// import ToolExperienceGraphBlock from 'core/blocks/tools/ToolExperienceGraphBlock'
-// import ToolsSectionOverviewBlock from 'core/blocks/tools/ToolsSectionOverviewBlock'
-// import ToolsMatricesBlock from 'core/blocks/tools/ToolsMatricesBlock'
-// import { ToolsUsageVariationsBlock } from 'core/blocks/tools/ToolsUsageVariationsBlock'
-
-// brackets
-import BracketMatchupsBlock from 'core/blocks/brackets/BracketMatchupsBlock'
-import BracketWinsBlock from 'core/blocks/brackets/BracketWinsBlock'
-
-// happiness
-// import { HappinessHistoryBlock } from 'core/blocks/happiness/HappinessHistoryBlock'
-
-// opinions
-// import OpinionBlock from 'core/blocks/opinions/OpinionBlock'
-
-/**
- * Please make sure to comment out unused blocks,
- * this is gonna help regarding bundle size.
- */
-const blockRegistry = {
-    // generic chart blocks
-    HorizontalBarBlock,
-    VerticalBarBlock,
-
-    // other
+const otherBlocks = {
     PageIntroductionBlock,
     TextBlock,
     RecommendedResourcesBlock,
+    TshirtBlock,
     SurveyIntroBlock,
     AwardsBlock,
     ConclusionBlock,
@@ -91,34 +103,9 @@ const blockRegistry = {
     AboutBlock,
     HowToHelpBlock,
     SponsorFinishBlock,
-    // ReportBlock,
-    
-    // demographics
-    ParticipationByCountryBlock,
-    GenderBlock,
-    ByFacetBlock,
+    LivestreamBlock,
 
-    // features
-    FeaturesOverviewBlock,
-    KnowledgeScoreBlock,
-    MultiFeaturesExperienceBlock,
-
-    // tools
-    ToolExperienceBlock,
-    ToolsScatterplotBlock: ToolsQuadrantsBlock,
-    ToolsExperienceMarimekkoBlock,
-    AllSectionsToolsCardinalityByUserBlock,
-    ToolsExperienceRadarBlock,
-    ToolsExperienceRankingSingleYearBlock,
-    // ToolExperienceGraphBlock,
-    // ToolsSectionOverviewBlock,
-    // ToolsMatricesBlock,
-    // ToolsUsageVariationsBlock,
-
-    // brackets
-    BracketMatchupsBlock,
-    BracketWinsBlock,
-
+    DataExplorerBlock
 }
 
-export default blockRegistry
+export default { ...chartBlocks, ...otherBlocks }
