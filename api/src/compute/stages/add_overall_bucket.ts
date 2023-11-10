0249ad6092e762789451c49f375a8dbc5b4f18f2
Sacha Greif <git@sachagreif.com>
2023-11-10 10:46:43 +0900

    📡 filter our no answer facet bucket from overall bucket; remove likert scale options from facets

--- api/src/compute/stages/add_overall_bucket.ts
+++ api/src/compute/stages/add_overall_bucket.ts
@@ -1,14 +1,21 @@
 import { ResponseEditionData, ResponsesTypes } from '@devographics/types'
 import { GenericComputeOptions, convertOrderReverse, genericComputeFunction } from '../generic'
 import { ComputeAxisParameters } from '../../types'
-import { OVERALL } from '@devographics/constants'
+import { NO_ANSWER, OVERALL } from '@devographics/constants'
 
-// create "overall" bucket using overall data as facets
+/*
+
+Create "overall" bucket using overall data as facets
+
+Note: filter out no answer bucket to avoid going over 100%
+when adding all facets' percentageBucket
+
+*/
 export const getOverallBucket = (overallEditionData: ResponseEditionData) => {
     const overallBucket = {
         id: OVERALL,
         count: overallEditionData.completion.count,
-        facetBuckets: overallEditionData.buckets
+        facetBuckets: overallEditionData.buckets.filter(bucket => bucket.id !== NO_ANSWER)
     }
     return overallBucket
 }
@@ -38,6 +45,12 @@ export const addOverallBucket = async (
         }
     }
     const overallResults = await genericComputeFunction(newOptions)
+
+    if (overallResults.length === 0) {
+        console.log({ axis })
+        console.log({ options })
+        throw new Error(`addOverallBucket: no overall results found`)
+    }
     for (let editionData of resultsByEdition) {
         // for each edition, find corresponding edition in overall results
         const editionOverallResults = overallResults.find(
