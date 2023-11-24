2c554937f61fbdd6d1b3ea4dbdf8e52b21c5e6fc
eric-burel <eb@lebrun-burel.com>
2023-11-24 10:42:13 +0100

    show result of API actions

--- surveyadmin/src/components/cache/Api.tsx
+++ surveyadmin/src/components/cache/Api.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { runScript } from "~/lib/scripts/services";
 
 export const actions = [
@@ -21,12 +21,11 @@ export const actions = [
 export const APIDashboard = () => (
   <div>
     <h3>API</h3>
-    {/* <p>
-      Those actions will tell the API to reload the GitHub repositories
-      containing the corresponding data
+    <p>Each button tell the API to reload relevant data.</p>
+    <p>
+      The API will in turn send a request to the Surveyform to refresh static
+      pages and its own caches.
     </p>
-    <p>This does NOT empty the Surveyform Redis cache</p> */}
-
     <div className="api-dashboard">
       <section>
         <h4>Development</h4>
@@ -64,19 +63,63 @@ export const APIDashboard = () => (
   </div>
 );
 
+const actionReducer = (state, action) => {
+  switch (action.type) {
+    case "init": {
+      return { loading: true, result: null, error: null };
+    }
+    case "error": {
+      return { loading: false, result: null, error: action.payload };
+    }
+    case "done": {
+      return { loading: false, result: action.payload, error: null };
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+};
+
+const MaybeJson = ({ data }: { data: any }) => {
+  if (!data) return <p>No data</p>;
+  try {
+    const res = JSON.stringify(data, null, 2);
+    return (
+      <p>
+        <code>
+          <pre>{res}</pre>
+        </code>
+      </p>
+    );
+  } catch (err) {
+    return <p>{data.toString()}</p>;
+  }
+};
 const Action = ({ label, script, target }) => {
-  const [loading, setLoading] = useState(false);
+  const [{ loading, result, error }, dispatch] = useReducer(actionReducer, {
+    loading: false,
+    result: null,
+    error: null,
+  });
   const handleClick = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const result = await runScript({ id: script, scriptArgs: { target } });
-    console.log(result);
-    setLoading(false);
+    try {
+      e.preventDefault();
+      dispatch({ type: "init" });
+      const result = await runScript({ id: script, scriptArgs: { target } });
+      dispatch({ type: "done", payload: result });
+      console.log(result);
+    } catch (err) {
+      dispatch({ type: "error", payload: error });
+    }
   };
   return (
-    <a role="button" href="#" aria-busy={loading} onClick={handleClick}>
-      {label}
-    </a>
+    <>
+      <a role="button" href="#" aria-busy={loading} onClick={handleClick}>
+        {label}
+      </a>
+      {result && <MaybeJson data={result} />}
+      {error && <p>{error}</p>}
+    </>
   );
 };
 export default APIDashboard;
