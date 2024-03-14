6f11fc60280e518e78fc749d066d14855a255b19
eric-burel <eb@lebrun-burel.com>
2024-03-14 16:43:29 +0100

    can load locale strings

--- shared/i18n/server/graphql.ts
+++ shared/i18n/server/graphql.ts
@@ -1,17 +1,12 @@
 const convertToGraphQLEnum = (s: string) => s.replace('-', '_')
 
-// TODO: move to locales specific code
 export const getLocalesQuery = (contexts: Array<string>, loadStrings = true) => {
     const args: Array<string> = []
-    // if (localeIds.length > 0) {
-    //     args.push(`localeIds: [${localeIds.map(convertToGraphQLEnum).join(',')}]`)
-    // }
     if (contexts.length > 0) {
         args.push(`contexts: [${contexts.join(', ')}]`)
     }
 
     const argumentsString = args.length > 0 ? `(${args.join(', ')})` : ''
-
     return `
 query {
         locales${argumentsString} {
