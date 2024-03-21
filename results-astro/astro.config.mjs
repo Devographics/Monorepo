982f02e0192adf8763374825f315e40c0889faaa
eric-burel <eb@lebrun-burel.com>
2024-03-21 15:28:11 +0100

    setup vercel adapter

--- results-astro/astro.config.mjs
+++ results-astro/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 // @see https://docs.astro.build/en/recipes/add-yaml-support/
 import yaml from '@rollup/plugin-yaml';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
@@ -11,4 +13,11 @@ export default defineConfig({
     // black box i18n redirection are hard to decipher
     // and show an eratic behaviour during dev
     // Use Astro.params to access the current locale easily
+    ,
+    // We use incremental rendering = SSR but cached on 1st request
+    // TODO: test the static alternative?
+    output: "server",
+    adapter: vercel({
+        isr: true
+    })
 });
