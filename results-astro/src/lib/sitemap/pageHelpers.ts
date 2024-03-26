19d8609a997135205f5884f8abefa9815541b5da
eric-burel <eb@lebrun-burel.com>
2024-03-26 17:44:14 +0100

    convert PageFooter and dependencies

--- results-astro/src/lib/sitemap/pageHelpers.ts
+++ results-astro/src/lib/sitemap/pageHelpers.ts
@@ -1,4 +1,28 @@
+// @see results/src/core/helpers/pageHelpers.ts
+import type { StringTranslator } from "@devographics/i18n"
 import type { PageDefinition } from "./sitemap"
+import type { EditionMetadata } from "@devographics/types"
 // TODO: get pages from the sitemap
 export const getPageLabelKey = (pageContext: PageDefinition): string =>
     pageContext.titleId || `sections.${pageContext.intlId || pageContext.id}.title`
+
+export const getPageLabel = ({
+    survey,
+    pageContext,
+    t,
+    options = { includeWebsite: false }
+}: {
+    survey: EditionMetadata,
+    pageContext: PageDefinition,
+    t: StringTranslator
+    options?: { includeWebsite?: boolean }
+}) => {
+    let label
+
+    label = t(getPageLabelKey(pageContext))?.t
+    if (options.includeWebsite === true) {
+        label = `${survey.survey.name} ${survey.year}: ${label}`
+    }
+
+    return label || ''
+}
