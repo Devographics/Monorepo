621f2a88c2c910fa48b12bca55ae402ce8ee8564
eric-burel <eb@lbke.fr>
2024-04-11 18:00:56 +0200

    clarify that it's a server-only layout

--- results-astro/src/components/ClientOnlyLayout.tsx
+++ results-astro/src/components/ClientOnlyLayout.tsx
@@ -4,11 +4,14 @@ import type React from "react"
 /**
  * Initialize dynamic global values such as the theme
  * client-side
- */
-export function ClientLayout({ children }: { children: React.ReactNode }) {
-    console.log("THEME INIT")
+ * 
+ * To be loaded using "client:only" to avoid duplicating the side-effect on the server
+ * 
     // TODO: how to avoid the redundancy with [...path].astro setup?
     // https://discord.com/channels/830184174198718474/1019713903481081876/threads/1228011232032849931
+ */
+export function ClientOnlyLayout({ children }: { children: React.ReactNode }) {
+    console.log("THEME INIT")
     initTheme()
     return <>{children}</>
 }
