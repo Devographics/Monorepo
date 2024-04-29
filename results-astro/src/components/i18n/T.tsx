cde0f6da35ea374ca57267be1dc79c5da6c50f4e
eric-burel <eb@lbke.fr>
2024-04-29 19:01:35 +0200

    demo of a translation mode

--- results-astro/src/components/i18n/T.tsx
+++ results-astro/src/components/i18n/T.tsx
@@ -1,4 +1,6 @@
+import { TK_ATTR } from "./attributes";
+
 /**
  * TODO: we cannot yet inject translations into React world, see Astro version
  */
-export function T({ k }: { k: string }) { return <span>React translation key {k}</span> }
+export function T({ k }: { k: string }) { return <span {...{ [TK_ATTR]: k }}>React translation key {k}</span> }
---
+++ results-astro/src/components/i18n/attributes.ts
@@ -0,0 +1,5 @@
+/** token key for a given translated text
+ * used to enable translation mode
+ */
+export const TK_DATASET = "dgTk"
+export const TK_ATTR = "data-dg-tk"
