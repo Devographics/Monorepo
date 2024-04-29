7133515aa2946e9186d485707cd7f56fbc77868d
eric-burel <eb@lbke.fr>
2024-04-29 19:07:46 +0200

    try to set styled component as an exeternal in remix

--- results-remix/vite.config.ts
+++ results-remix/vite.config.ts
@@ -9,4 +9,8 @@ installGlobals();
 // preprocessor don't need a plugin @see https://vitejs.dev/guide/features#css-pre-processors
 export default defineConfig({
   plugins: [remix(), yaml(), tsconfigPaths()],
+  ssr: {
+    // might fix the issue in Astro but sadly it failed here: https://stackblitz.com/edit/withastro-astro-hp4513?file=astro.config.mjs
+    noExternal: ["styled-components"]
+  }
 });
