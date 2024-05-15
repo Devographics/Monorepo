f86c1aac5d4e57341f73ec2cdf71d34df61904ec
eric-burel <eb@lbke.fr>
2024-05-15 18:00:19 +0200

    use children as fallback (for english translation) in T component

--- results-astro/src/components/i18n/T.tsx
+++ results-astro/src/components/i18n/T.tsx
@@ -13,23 +13,45 @@ export type TProps = {
      * 
      * - Cannot be a token expression with a range like "[[options]].foobar" 
      * (such a token doesn't make sense, it should be a specific value like "option_1.foobar")
+     * 
+     * TODO: should we accept multiple tokens for fallback?
      */
     token: string,
     /**
      * Use the HTML instead of the default "t" value
      * TODO: not yet actually used, not sure of use cases at this point, need to check existing code
      */
-    mode?: "clean" | "html"
-} & React.HTMLProps<HTMLSpanElement>
+    mode?: "clean" | "html",
+    /**
+     * Allow to setup a default translation in English
+     * If used, attribute "data-dg-fallback-children=true" will be added
+     */
+    children?: React.ReactNode,
+    ///** Switch between div and span */
+    tag?: "div" | "span"
+}
+    // Approximation to allow other common props
+    & React.HTMLProps<HTMLSpanElement> & React.HTMLProps<HTMLDivElement>
+
 /**
  * TODO: we cannot yet inject translations into React world, see Astro version
  */
 export function T({
     token,
     mode,
+    tag = "span",
+    children,
     ...otherProps
 }: TProps) {
     const { getString } = useI18n()
     const value = getString(token)
-    return <span {...{ [TK_ATTR]: token }} {...otherProps}>{value.t}</span>
+    const Tag = tag // uppercase for JSX
+    const displayedValue = value.t
+    let useFallbackChildren = !displayedValue && !!children
+    // having a wrapper element is mandatory to attach some metadata
+    // helping live translation later on
+    return <Tag {...{
+        [TK_ATTR]: token,
+        "data-dg-fallback-children": useFallbackChildren ? "true" : undefined
+    }} {...otherProps}>{displayedValue || children}</Tag>
 }
