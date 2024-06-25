29ce4d67e015817ddbfa4d0a4cabdaa00df05bee
Sacha Greif <git@sachagreif.com>
2024-06-25 11:27:17 +0900

    ⚙️ fix toolv3 template issue for now

--- shared/templates/templates/text.ts
+++ shared/templates/templates/text.ts
@@ -5,13 +5,15 @@ import {
     QuestionTemplateOutput,
     TemplateFunction
 } from '@devographics/types'
-import { checkHasId } from '../helpers'
+import { checkHasId, isToolTemplate } from '../helpers'
 
 export const text: TemplateFunction = options => {
     const { edition, section } = options
     const question = checkHasId(options)
 
-    const sectionSegment = section.template === 'tool' ? 'tools_others' : section.slug || section.id
+    const sectionSegment = isToolTemplate(section?.template)
+        ? 'tools_others'
+        : section.slug || section.id
     const questionSegment = question.id
 
     // TODO: currently surveyform looks in "response" for all fields,
