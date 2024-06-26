3748ce91526a3b8cb111030ae03658c434579bd4
Sacha Greif <git@sachagreif.com>
2024-06-26 17:18:09 +0900

    📡 test webhooks

--- api/src/webhooks.ts
+++ api/src/webhooks.ts
@@ -1,21 +1,22 @@
-import { json, Router } from "express";
-import { checkMainPushAction, verifyGhWebhookMiddleware } from "./external_apis";
-import { reinitialize } from "./init";
-import { RequestContext } from "./types";
+import { json, Router } from 'express'
+import { checkMainPushAction, verifyGhWebhookMiddleware } from './external_apis'
+import { reinitialize } from './init'
+import { RequestContext } from './types'
 
 /**
  * Hooks to be registered on "/gh"
- * 
+ *
  * @see https://github.com/Devographics/surveys/settings/hooks
  * @see https://github.com/Devographics/locale-en-US/settings/hooks
  * @see https://github.com/Devographics/entities/settings/hooks
- * 
- * @param context 
+ *
+ * @param context
  * @returns an Express router to be registered on "/gh" endpoint
  */
 export function ghWebhooks(context: RequestContext): Router {
     const ghRouter = Router()
-    ghRouter.use(json(),
+    ghRouter.use(
+        json(),
         verifyGhWebhookMiddleware // important to prevent random people from forcing a reload
     )
 
@@ -23,44 +24,46 @@ export function ghWebhooks(context: RequestContext): Router {
      * @see https://docs.github.com/fr/webhooks
      * @see https://docs.github.com/en/webhooks/using-webhooks/securing-your-webhooks#typescript-example
      */
-    ghRouter.post("/reinitialize-surveys",
-        checkMainPushAction,
-        async function (req, res) {
-            console.log("Triggering surveys reinitialization from GitHub webhook")
-            await reinitialize({ context, initList: ['surveys'] })
-            // TODO: check if the push in on main branch
-            return res.status(200).send("Surveys reloaded")
-        })
+    ghRouter.post('/reinitialize-surveys', checkMainPushAction, async function (req, res) {
+        console.log('Triggering surveys reinitialization from GitHub webhook')
+        await reinitialize({ context, initList: ['surveys'] })
+        // TODO: check if the push in on main branch
+        return res.status(200).send('Surveys reloaded')
+    })
     /**
-     * 
+     *
      * @see https://docs.github.com/fr/webhooks
      * @see https://docs.github.com/en/webhooks/using-webhooks/securing-your-webhooks#typescript-example
      */
-    ghRouter.post("/reinitialize-entities",
+    ghRouter.post(
+        '/reinitialize-entities',
         json(),
         verifyGhWebhookMiddleware, // important
         checkMainPushAction,
         async function (req, res) {
-            console.log("Triggering entities reinitialization from GitHub webhook")
+            console.log('Triggering entities reinitialization from GitHub webhook')
             await reinitialize({ context, initList: ['entities', 'surveys'] })
             // TODO: check if the push in on main branch
-            return res.status(200).send("Entities (and surveys) reloaded")
-        })
+            return res.status(200).send('Entities (and surveys) reloaded')
+        }
+    )
 
     /**
      * @see https://docs.github.com/fr/webhooks
      * @see https://docs.github.com/en/webhooks/using-webhooks/securing-your-webhooks#typescript-example
      */
-    ghRouter.post("/reinitialize-locales",
+    ghRouter.post(
+        '/reinitialize-locales-github',
         json(),
         verifyGhWebhookMiddleware, // important
         checkMainPushAction,
         async function (req, res) {
-            console.log("Triggering locales reinitialization from GitHub webhook")
+            console.log('🟣 Triggering locales reinitialization from GitHub webhook')
             await reinitialize({ context, initList: ['locales'] })
             // TODO: check if the push in on main branch
-            return res.status(200).send("Locales reloaded")
-        })
+            return res.status(200).send('Locales reloaded')
+        }
+    )
 
     return ghRouter
 }
