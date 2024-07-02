f7bace413b5fad3fecff73d6a6d3dec0320d989c
Sacha Greif <git@sachagreif.com>
2024-07-02 10:10:12 +0900

    ⚙️ update types

--- shared/react-i18n-legacy/Provider.tsx
+++ shared/react-i18n-legacy/Provider.tsx
@@ -1,69 +1,69 @@
-"use client";
+'use client'
 /**
  * Context that provides methods to access translated strings
  */
-import { useContext, createContext } from "react";
-import { I18nToken, StringsRegistry } from "./stringsRegistry";
-import { Message } from "./typings";
+import { useContext, createContext, ReactNode } from 'react'
+import { I18nToken, StringsRegistry } from './stringsRegistry'
+import { Message } from './typings'
 
 interface IntlContextValue {
-  localeId: string;
-  stringsRegistry: StringsRegistry;
+    localeId: string
+    stringsRegistry: StringsRegistry
 }
 export interface IntlContextFormat extends IntlContextValue {
-  formatMessage: (msg: Message) => I18nToken;
+    formatMessage: (msg: Message) => I18nToken
 }
 
 const makeContext = ({
-  localeId,
-  stringsRegistry,
+    localeId,
+    stringsRegistry
 }: {
-  localeId: string;
-  stringsRegistry: StringsRegistry;
+    localeId: string
+    stringsRegistry: StringsRegistry
 }): IntlContextFormat => ({
-  localeId,
-  stringsRegistry,
-  formatMessage: ({ id, defaultMessage, values }: Message) => {
-    const message = stringsRegistry.getString({
-      id,
-      defaultMessage,
-      values,
-      localeId,
-    });
-    return message;
-  },
-});
+    localeId,
+    stringsRegistry,
+    formatMessage: ({ id, defaultMessage, values }: Message) => {
+        const message = stringsRegistry.getString({
+            id,
+            defaultMessage,
+            values,
+            localeId
+        })
+        return message
+    }
+})
 
 export const IntlContext = createContext<IntlContextFormat>(
-  // default values: return tokens as is
-  makeContext({
-    localeId: "NOT_INITIALIZED",
-    stringsRegistry: new StringsRegistry("NOT_INITIALIZED"),
-  }),
-);
+    // default values: return tokens as is
+    makeContext({
+        localeId: 'NOT_INITIALIZED',
+        stringsRegistry: new StringsRegistry('NOT_INITIALIZED')
+    })
+)
 
 export interface IntlProviderProps extends IntlContextValue {
-  children: React.ReactNode;
+    children: ReactNode
 }
 export const IntlContextProvider = ({
-  localeId,
-  stringsRegistry,
-  children,
-  ...props
+    localeId,
+    stringsRegistry,
+    children,
+    ...props
 }: IntlProviderProps) => {
-  // merge parent strings if any (default registry is just empty)
-  const currentContext = useIntlContext();
-  stringsRegistry.mergeTokens(currentContext.stringsRegistry);
+    // merge parent strings if any (default registry is just empty)
+    const currentContext = useIntlContext()
+    stringsRegistry.mergeTokens(currentContext.stringsRegistry)
 
-  const formatters = makeContext({ localeId, stringsRegistry });
-  return (
-    <IntlContext.Provider value={formatters} {...props}>
-      {children}
-    </IntlContext.Provider>
-  );
-};
+    const formatters = makeContext({ localeId, stringsRegistry })
+    return (
+        <IntlContext.Provider value={formatters} {...props}>
+            {children}
+        </IntlContext.Provider>
+    )
+}
 
 /**
  * @deprecated
  */
-export const useIntlContext = () => useContext(IntlContext);
+export const useIntlContext = () => useContext(IntlContext)
--- shared/react-i18n/DynamicT.tsx
+++ shared/react-i18n/DynamicT.tsx
@@ -1,49 +1,49 @@
-"use client"
-import { useTeapot } from "./i18nContext";
-import { DATA_FALLBACK_CHILDREN_ATTR, DATA_MISSING_ATTR, DATA_TOKEN_ATTR } from "@devographics/i18n";
-import React from "react";
+'use client'
+import { useTeapot } from './i18nContext'
+import { DATA_FALLBACK_CHILDREN_ATTR, DATA_MISSING_ATTR, DATA_TOKEN_ATTR } from '@devographics/i18n'
+import React, { ReactNode } from 'react'
 
 export type TProps = {
     /**
      * Token key
-     * 
+     *
      * Not named "key" because confusion with React "key" which is reserved
-     * 
+     *
      * - Can be a token expression with a contextual value like "{{surveyId}}.foobar"
      * (since they are resolved at build-time, the component doesn't care about the actual current surveyId)
-     * 
-     * - Cannot be a token expression with a range like "[[options]].foobar" 
+     *
+     * - Cannot be a token expression with a range like "[[options]].foobar"
      * (such a token doesn't make sense, it should be a specific value like "option_1.foobar")
-     * 
+     *
      * TODO: should we accept multiple tokens for fallback?
      */
-    token: string,
-    values?: Record<string, any>,
+    token: string
+    values?: Record<string, any>
     /**
      * Fallback string
      * If you need a React component as fallback, use "children" instead
      */
-    fallback?: string,
+    fallback?: string
     /**
      * Use the HTML instead of the default "t" value
      * TODO: not yet actually used, not sure of use cases at this point, need to check existing code
      */
-    mode?: "clean" | "html",
+    mode?: 'clean' | 'html'
     /**
      * Allow to setup a default translation in English
      * If used, attribute "data-dg-fallback-children=true" will be added
      */
-    children?: React.ReactNode,
+    children?: ReactNode
     ///** Switch between div and span */
-    tag?: "div" | "span"
-}
-    // Approximation to allow other common props
-    & React.HTMLProps<HTMLSpanElement> & React.HTMLProps<HTMLDivElement>
+    tag?: 'div' | 'span'
+} & // Approximation to allow other common props
+React.HTMLProps<HTMLSpanElement> &
+    React.HTMLProps<HTMLDivElement>
 
 /**
  * Do not use in your app,
  * use DynamicT
- * 
+ *
  * Keep code in sync with ServerT
  */
 export function InternalT({
@@ -51,7 +51,7 @@ export function InternalT({
     values,
     fallback,
     mode,
-    tag = "span",
+    tag = 'span',
     children,
     ...otherProps
 }: TProps) {
@@ -72,16 +72,18 @@ export function InternalT({
     if (message.missing) {
         // Try to use children value as fallback
         if (fallback && children) {
-            console.warn(`Ambiguous fallback for token ${token}. Use either "fallback" props or React "children", but not both. Will use "fallback" prop.`)
+            console.warn(
+                `Ambiguous fallback for token ${token}. Use either "fallback" props or React "children", but not both. Will use "fallback" prop.`
+            )
             wrapperProps[DATA_MISSING_ATTR] = true
-            return <span {...wrapperProps}> {message.t}</span >
+            return <span {...wrapperProps}> {message.t}</span>
         } else if (children) {
             wrapperProps[DATA_MISSING_ATTR] = true
             wrapperProps[DATA_FALLBACK_CHILDREN_ATTR] = true
-            return <span {...wrapperProps}>{children}</span >
+            return <span {...wrapperProps}>{children}</span>
         } else {
             // "getMessage" already put "fallback" into "t" for us
-            return <span {...wrapperProps}>{message.t}</span >
+            return <span {...wrapperProps}>{message.t}</span>
         }
     }
     return <span {...wrapperProps}>{message.t}</span>
@@ -93,17 +95,17 @@ export function InternalT({
  * Translation component, for tokens that are not known ahead of time
  * This is typically used for low-level UI components
  * or generated UIs such as form based on a yaml file
- * 
+ *
  * @example function MenuItem({menuToken}) { return <div><DynamicT token={menuToken} /></div> }
- * 
+ *
  * Use only as a last resort for truely dynamic tokens
- * 
+ *
  * Favour passing the rendered text from parent component instead,
  * so your UI component don't have to think about i18n,
- * In the parent, use the "teapot" function with token expressions 
+ * In the parent, use the "teapot" function with token expressions
  * @example function MenuItem({menuText}) { return <div>{menuText}</div> }
  * // define menuText from parent
- * 
+ *
  *
  */
 export const DynamicT = InternalT
