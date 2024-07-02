9976680f0fed37e2f9b5bbdc5ff8cb718d8b8fa7
Sacha Greif <git@sachagreif.com>
2024-07-02 15:06:16 +0900

    ⚙️ add new DEFAULT_LOCALE_CONTEXTS env var

--- shared/helpers/utils.ts
+++ shared/helpers/utils.ts
@@ -34,3 +34,14 @@ export function concatPath(...segments: string[]) {
         }
     })
 }
+
+/**
+ * Example:
+ * APOLLO_SERVER_CORS_WHITELIST=http://localhost:5000,https://www.my-client.org
+ * => parse the string and makes it an array
+ * @param {*} variable Env array variables, with values separated by a comma (spaces allowed)
+ */
+export const parseEnvVariableArray = (variable = '') => {
+    if (!variable) return []
+    return variable.split(',').map(s => s.trim())
+}
