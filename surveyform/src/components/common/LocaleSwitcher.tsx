e8a821a50723c446206153b5bdca819ebb089a2c
Sacha Greif <git@sachagreif.com>
2024-07-19 15:31:05 +0900

    ✍️ fix error

--- surveyform/src/components/common/LocaleSwitcher.tsx
+++ surveyform/src/components/common/LocaleSwitcher.tsx
@@ -3,30 +3,30 @@
 import { useLocaleContext } from "~/i18n/context/LocaleContext";
 import { Dropdown } from "~/components/ui/Dropdown";
 
-
-
-import { T } from "@devographics/react-i18n"
+import { T } from "@devographics/react-i18n";
 import { useParams, usePathname, useRouter } from "next/navigation";
 
 /**
  * We suppose the current locale is known and is in the URL
  * "/fr-FR"
  * "/fr-FR/foobar"
- * 
+ *
  * There is no method to just remove a param from the router pathname yet
  */
 function replaceLocale(pathname: string, params: any, newLocale: string) {
   if (!(params.lang && typeof params.lang === "string")) {
-    console.warn(`No params found when switching locale, will append the newly selected locale to the pathname "${pathname}"`)
-    return `/${newLocale}${pathname}`
+    console.warn(
+      `No params found when switching locale, will append the newly selected locale to the pathname "${pathname}"`
+    );
+    return `/${newLocale}${pathname}`;
   }
-  return pathname.replace(params.lang, newLocale)
+  return pathname.replace(params.lang, newLocale);
 }
 
 const LocaleSwitcher = () => {
-  const router = useRouter()
-  const params = useParams()
-  const pathname = usePathname()
+  const router = useRouter();
+  const params = useParams();
+  const pathname = usePathname();
   const { locales = [] } = useLocaleContext();
   const { locale: currentLocale, setLocale } = useLocaleContext();
 
@@ -38,8 +38,9 @@ const LocaleSwitcher = () => {
         variant: "default",
       }}
       label={
-        currentLocale?.label || (currentLocale?.id && <T
-          token={currentLocale?.id} />) || "Please select a locale"
+        currentLocale?.label ||
+        (currentLocale?.id && <T token={currentLocale?.id} />) ||
+        "Please select a locale"
       }
       id="locale-dropdown"
       className="nav-locale-dropdown"
@@ -48,9 +49,9 @@ const LocaleSwitcher = () => {
         if (!index) {
           index = 0;
         }
-        const locale = locales[index]
+        const locale = locales[index];
         setLocale(locale.id);
-        router.replace(replaceLocale(pathname, params, locale.id))
+        router.replace(replaceLocale(pathname || "", params, locale.id));
       }}
       menuItems={locales.map(({ label, id }) => ({
         label: id === currentLocale?.id ? `${label} ✓` : label,
