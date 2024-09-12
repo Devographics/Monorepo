dd3eb158f9e96eb13cfc0e8bbc040e905715120f
Sacha Greif <git@sachagreif.com>
2024-09-12 20:39:55 +0900

    ⚙️ fix opinion template

--- shared/templates/templates/opinion.ts
+++ shared/templates/templates/opinion.ts
@@ -1,10 +1,13 @@
-import { QuestionTemplateOutput, TemplateFunction } from '@devographics/types'
+import { DbSuffixes, QuestionTemplateOutput, TemplateFunction } from '@devographics/types'
 import { getPaths, checkHasId } from '../helpers'
 
 export const opinion: TemplateFunction = options => {
     checkHasId(options)
 
     const question = {
+        inputComponent: 'single',
+        allowMultiple: false,
+        defaultSort: 'options',
         optionsAreNumeric: true,
         options: [...Array(5)].map((x, i) => ({
             id: i,
@@ -14,7 +17,7 @@ export const opinion: TemplateFunction = options => {
     } as QuestionTemplateOutput
 
     const output: QuestionTemplateOutput = {
-        ...getPaths({ ...options, question }),
+        ...getPaths({ ...options, question }, DbSuffixes.CHOICES),
         ...question
     }
 
