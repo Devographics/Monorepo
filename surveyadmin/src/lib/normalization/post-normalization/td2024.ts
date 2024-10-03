3b8eb989bf5522a1f58ca1b06c4372f814b6b108
Sacha Greif <git@sachagreif.com>
2024-10-03 15:33:22 +0900

    🔒 td2024 post-norm operations

--- surveyadmin/src/lib/normalization/post-normalization/td2024.ts
+++ surveyadmin/src/lib/normalization/post-normalization/td2024.ts
@@ -1,3 +1,4 @@
+import { getNormResponsesCollection } from "@devographics/mongo";
 import { deleteDuplicates } from "../helpers/deleteDuplicates";
 import { generateSourceField } from "../helpers/generateSourceField";
 import { PostNormalizationOperationOptions, runOperation } from "./common";
@@ -8,5 +9,20 @@ export const td2024 = async (options: PostNormalizationOperationOptions) => {
   await runOperation(generateSourceField, options, operationResults);
   await runOperation(deleteDuplicates, options, operationResults);
   await runOperation(removeNonJapanResponses, options, operationResults);
+  await runOperation(removeOve140HoursPerWeek, options, operationResults);
   return operationResults;
 };
+
+export async function removeOve140HoursPerWeek({
+  edition,
+}: PostNormalizationOperationOptions) {
+  const normalizedResponses = await getNormResponsesCollection();
+  const path = "job_info.work_hours_per_week.choices";
+  const selector = {
+    editionId: edition.id,
+    [path]: { $gt: 140 },
+  };
+  return await normalizedResponses.updateMany(selector, {
+    $unset: { [path]: 1 },
+  });
+}
