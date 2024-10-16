a20b8b414dd9b2426a48dd87ed078ce9b643b998
Sacha Greif <git@sachagreif.com>
2024-10-16 15:53:36 +0900

    📊 add webFeatures query fragments

--- results/src/core/queries/fragments/getEntityFragment.ts
+++ results/src/core/queries/fragments/getEntityFragment.ts
@@ -54,4 +54,24 @@ export const getEntityFragment = () => `entity {
         url
         title
     }
+    webFeature {
+        id
+        description_html
+        group
+        name
+        spec
+        status {
+            baseline
+            baseline_low_date
+            support {
+            chrome
+            chrome_android
+            edge
+            firefox
+            firefox_android
+            safari
+            safari_ios
+            }
+        }
+    }
 }`
