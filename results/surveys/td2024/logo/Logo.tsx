c4320ec293aebef49a1081f696d8a58f5de2cb7d
Sacha Greif <git@sachagreif.com>
2024-10-23 11:35:01 +0900

    📊 td2024 fixes

--- results/surveys/td2024/logo/Logo.tsx
+++ results/surveys/td2024/logo/Logo.tsx
@@ -12,8 +12,8 @@ interface LogoProps {
 export const Logo = ({ className, animated = true, showText = true, size = 'l' }: LogoProps) => (
     <Wrapper>
         <img
-            src="https://assets.surveys.tokyodev.com/images/surveys/td2023-wide.png"
-            alt="TokyoDev Developer Survey 2023"
+            src="https://assets.surveys.tokyodev.com/images/surveys/td2024-wide.png"
+            alt="TokyoDev Developer Survey 2024"
         />
     </Wrapper>
 )
