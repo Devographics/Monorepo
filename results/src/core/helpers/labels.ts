610bf62be4bb60e253d9fecec1dd83f6292a6c01
Sacha Greif <git@sachagreif.com>
2024-12-20 09:40:34 +0900

    📊 tweak label fallback logic

--- results/src/core/helpers/labels.ts
+++ results/src/core/helpers/labels.ts
@@ -12,6 +12,7 @@ import { StringTranslator } from '@devographics/react-i18n'
 import { Entity } from '@devographics/types'
 
 const predefinedKeys: { [key: string]: string } = {
+    [NOT_APPLICABLE]: 'options.na',
     [NO_ANSWER]: 'charts.no_answer',
     [INSUFFICIENT_DATA]: 'charts.insufficient_data',
     [OVERALL]: 'charts.overall',
@@ -21,12 +22,23 @@ const predefinedKeys: { [key: string]: string } = {
     [OVERLIMIT_ANSWERS]: 'charts.overlimit_answers'
 }
 
+enum LabelSourcesEnum {
+    ID = 'id',
+    PREDEFINED = 'predefined',
+    ENTITY = 'entity',
+    ENTITY_I18N = 'entity_i18n',
+    I18N = 'i18n',
+    PROVIDED = 'provided'
+}
+
 export type LabelObject = {
     key?: string
     label: string
     shortLabel: string
+    descriptionKey?: string
     description?: string
     values?: any
+    source: LabelSourcesEnum
 }
 
 const getFields = (item: any, fields: string[]) => {
@@ -63,49 +75,102 @@ export const getItemLabel = (options: {
         html = false
     } = options
 
-    let key, label, shortLabel, genericLabel, description
+    // 1. use id as default
+    let key = String(id),
+        label = String(id),
+        shortLabel,
+        description,
+        descriptionKey,
+        source = LabelSourcesEnum.ID
 
-    if (providedLabel) {
-        // if a label is provided, use that
-        label = providedLabel
-        shortLabel = providedLabel
-    } else {
-        // some ids have generic labels that work for all questions
-        if (id === NOT_APPLICABLE) {
-            genericLabel = getString('options.na')?.t
+    // 2. if this is a predefined key, use that
+    if (Object.keys(predefinedKeys).includes(String(id))) {
+        // 2. this is special field with a predefined key
+        key = predefinedKeys[id]
+        label = getString(key)?.t
+        shortLabel = label
+        source = LabelSourcesEnum.PREDEFINED
+    }
+
+    // 3. look for entity metadata
+    if (entity) {
+        key = 'entity'
+        label = getFields(entity, ['alias', html ? 'nameHtml' : 'nameClean', 'name']) as string
+        description = getFields(entity, [
+            html ? 'descriptionHtml' : 'descriptionClean',
+            'description'
+        ])
+        source = LabelSourcesEnum.ENTITY
+    }
+
+    // 4. look for entity translation
+    if (entity) {
+        const entityLabelKey = `entities.${id}.name`
+        const entityLabelObject = getString(entityLabelKey, { values })
+        if (!entityLabelObject.missing) {
+            key = entityLabelKey
+            label = getFields(entityLabelObject, [html ? 'tHtml' : 'tClean', 't']) as string
+            source = LabelSourcesEnum.ENTITY_I18N
         }
 
-        // else, try using an i18n key
-        // TODO: get rid of these exceptions?
-        // note: make an exception for "features" namespace because it does not use the
-        // "options." prefix
-        const defaultKey =
-            i18nNamespace === 'tools'
-                ? `tools.${id}`
-                : i18nNamespace === 'features'
-                ? `features.${id}`
-                : `options.${i18nNamespace}.${id}`
-        const predefinedKey = predefinedKeys[id]
-
-        key = predefinedKey || defaultKey
-        const descriptionKey = `${key}.description`
-
-        const i18nLabelObject = getString(key, { values })
-        const i18nLabel = getFields(i18nLabelObject, [html ? 'tHtml' : 'tClean', 't'])
-        const entityName =
-            entity && getFields(entity, ['alias', html ? 'nameHtml' : 'nameClean', 'name'])
-
-        const shortLabelObject = getString(key + '.short', { values }, i18nLabel)
-
-        const i18nDescriptionObject = getString(descriptionKey, { values })
-        const i18nDescription = getFields(i18nDescriptionObject, [html ? 'tHtml' : 'tClean', 't'])
-        const entityDescription =
-            entity &&
-            getFields(entity, [html ? 'descriptionHtml' : 'descriptionClean', 'description'])
-
-        label = String(i18nLabel || entityName || genericLabel || id)
-        shortLabel = getFields(shortLabelObject, [html ? 'tHtml' : 'tClean', 't']) || label
-        description = i18nDescription || entityDescription
+        const entityDescriptionKey = `entities.${id}.description`
+        const entityDescriptionObject = getString(entityDescriptionKey, { values })
+        if (!entityDescriptionObject.missing) {
+            descriptionKey = entityDescriptionKey
+            description = getFields(entityDescriptionObject, [
+                html ? 'tHtml' : 'tClean',
+                't'
+            ]) as string
+        }
     }
-    return { key, label, shortLabel, description, values }
+
+    // 5. look for regular translation
+
+    // TODO: get rid of these exceptions?
+    // note: make an exception for "features" and "tools" namespaces
+    // because they do not use the "options." prefix
+    let namespace
+    if (i18nNamespace === 'tools') {
+        namespace = 'tools'
+    } else if (i18nNamespace === 'features') {
+        namespace = 'features'
+    } else {
+        namespace = `options.${i18nNamespace}`
+    }
+
+    const i18nLabelKey = `${namespace}.${id}`
+    const i18nLabelObject = getString(i18nLabelKey, { values })
+    if (!i18nLabelObject.missing) {
+        key = i18nLabelKey
+        label = getFields(i18nLabelObject, [html ? 'tHtml' : 'tClean', 't']) as string
+        source = LabelSourcesEnum.I18N
+    }
+
+    const i18nShortObject = getString(`${i18nLabelKey}.short`, { values })
+    if (!i18nShortObject.missing) {
+        shortLabel = getFields(i18nShortObject, [html ? 'tHtml' : 'tClean', 't']) as string
+    }
+
+    const i18nDescriptionKey = `${key}.description`
+    const i18nDescriptionObject = getString(i18nDescriptionKey, { values })
+    if (!i18nDescriptionObject.missing) {
+        key = i18nDescriptionKey
+        description = getFields(i18nDescriptionObject, [html ? 'tHtml' : 'tClean', 't']) as string
+    }
+
+    // 6. use provided label
+    if (providedLabel) {
+        // 1. if a label is provided, use that
+        key = 'provided'
+        label = providedLabel
+        shortLabel = providedLabel
+        source = LabelSourcesEnum.PROVIDED
+    }
+
+    // if shortLabel has not been defined, just set it to the label
+    if (!shortLabel) {
+        shortLabel = label
+    }
+    const result = { key, label, shortLabel, descriptionKey, description, values, source }
+    return result
 }
