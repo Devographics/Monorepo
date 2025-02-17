b2564b750f83f835644eb7e4e2be9abc1f91f4b8
Sacha Greif <git@sachagreif.com>
2025-02-17 10:49:20 +0900

    📊 add banner

--- results/src/core/components/SurveyBanner.tsx
+++ results/src/core/components/SurveyBanner.tsx
@@ -8,36 +8,67 @@ import { useStickyState } from 'core/filters/helpers'
 
 const SurveyBanner = () => {
     const [showBanner, setShowBanner] = useStickyState(true, 'hideBanner')
+    const { bannerId, bannerLink, bannerBg, bannerButton, bannerText } = config
+
+    const style = {
+        '--bannerBg': bannerBg,
+        '--bannerButton': bannerButton,
+        '--bannerText': bannerText
+    }
+
     return config.bannerId && config.bannerLink && showBanner ? (
-        <Banner_ style={{ backgroundColor: config.bannerColor }}>
-            <a href={config.bannerLink} rel="noreferrer" target="_blank">
-                <T k={config.bannerId} md={true} html={true} />
-            </a>
-            <CloseIcon
-                onClick={() => {
-                    setShowBanner(false)
-                }}
-            />
-        </Banner_>
+        <div style={style}>
+            <Banner_>
+                <a href={bannerLink} rel="noreferrer" target="_blank">
+                    <span className="survey-ongoing">
+                        <T k="results.ongoing_survey" />
+                    </span>
+                    <T k={bannerId} md={true} html={true} />
+                </a>
+                <CloseIcon
+                    onClick={() => {
+                        setShowBanner(false)
+                    }}
+                />
+            </Banner_>
+        </div>
     ) : null
 }
 
 const Banner_ = styled.div`
     grid-area: banner;
     width: 100%;
-    background: var(--backgroundAltColor);
+    background: var(--bannerBg, var(--backgroundAltColor));
     text-align: center;
     position: relative;
     padding: 7px 10px;
     border-bottom: 1px dashed var(--borderColor);
-    a {
+    /* a {
         color: var(--textColor);
+    } */
+    a {
+        color: var(--bannerText);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: var(--halfSpacing);
+    }
+    div {
+        /* line-height: 1; */
     }
     .icon-wrapper {
         position: absolute;
         right: 10px;
         top: 50%;
         transform: translateY(-50%);
+    }
+    .survey-ongoing {
+        background: var(--bannerButton, var(--linkColor));
+        border-radius: 3px;
+        text-transform: uppercase;
+        color: var(--textColorInverted);
+        padding: 2px 6px;
+        font-size: 0.8rem;
     }
 `
 
