50e0e34294a078dd8ef220b6e3fdc73d5f142121
Sacha Greif <git@sachagreif.com>
2025-05-22 14:33:18 +0900

    ⚙️ icons

---
+++ results/src/core/charts/common2/comments/CommentsLikes.tsx
@@ -0,0 +1,29 @@
+import { HeartIcon, Heart2Icon } from '@devographics/icons'
+import Button from 'core/components/Button'
+import Tooltip from 'core/components/Tooltip'
+import T from 'core/i18n/T'
+import React from 'react'
+
+export const CommentsLikes = () => {
+    const hasLiked = false
+    return (
+        <div>
+            <Tooltip
+                trigger={
+                    <Button
+                        size="small"
+                        className={`comment-like comment-like-${hasLiked ? 'liked' : 'unliked'}`}
+                        onClick={() => {
+                            alert(1)
+                        }}
+                    >
+                        {hasLiked ? <Heart2Icon size="small" /> : <HeartIcon size="small" />}
+                        <span className="comment-like-count">9999</span>
+                    </Button>
+                }
+                showBorder={false}
+                contents={<T k="comment.like.description" />}
+            />
+        </div>
+    )
+}
---
+++ shared/icons/icons/Heart2.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import IconWrapper, { IconProps } from './IconWrapper'
+
+export const Heart2Icon = (props: IconProps) => (
+    <IconWrapper {...props}>
+        <svg
+            version="1.1"
+            x="0px"
+            y="0px"
+            width="24px"
+            height="24px"
+            viewBox="0 0 24 24"
+            enableBackground="new 0 0 24 24"
+            xmlSpace="preserve"
+            aria-hidden="true"
+            stroke="none"
+        >
+            <path
+                fill="currentColor"
+                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+            ></path>
+        </svg>
+    </IconWrapper>
+)
+
+export default Heart2Icon
