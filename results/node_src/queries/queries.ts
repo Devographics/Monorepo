9d95b5a248f23f15c8a576951263e8e11207917e
Sacha Greif <git@sachagreif.com>
2025-06-23 16:01:09 +0900

    📊 we do need block.queryOptions

--- results/node_src/queries/queries.ts
+++ results/node_src/queries/queries.ts
@@ -57,6 +57,7 @@ export const getBlockQuery = ({
     const questionId = axis1?.id || block.fieldId || block.id
 
     const queryOptions = {
+        ...block.queryOptions,
         surveyId: survey?.id,
         editionId: edition?.id,
         sectionId: axis1?.sectionId || section?.id,
