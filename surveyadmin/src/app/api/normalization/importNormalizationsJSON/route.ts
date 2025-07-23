4278f38d9de6b942e1e979bdf7aa04daef378d00
Sacha Greif <git@sachagreif.com>
2025-07-23 14:34:45 +0200

    🔒 try to fix deploy

--- surveyadmin/src/app/api/normalization/importNormalizationsJSON/route.ts
+++ surveyadmin/src/app/api/normalization/importNormalizationsJSON/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { captureException } from "@sentry/nextjs";
 import { importNormalizationsJSON } from "~/lib/normalization/actions";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     let body: any;
     try {
