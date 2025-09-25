c3f43ec969b4a2cfb1cd777813e82fcdbb60c7c5
Sacha Greif <git@sachagreif.com>
2025-09-25 10:49:06 +0900

    td2025

---
+++ results/surveys/td2025/logo/Logo.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import { mq, spacing, fontSize } from 'core/theme'
+
+interface LogoProps {
+    className?: string
+    animated?: boolean
+    size?: string
+    showText?: boolean
+}
+
+export const Logo = ({ className, animated = true, showText = true, size = 'l' }: LogoProps) => (
+    <Wrapper>
+        <img
+            src="https://assets.surveys.tokyodev.com/images/surveys/td2024-wide.png"
+            alt="TokyoDev Developer Survey 2024"
+        />
+    </Wrapper>
+)
+
+const Wrapper = styled.div`
+    margin: 0 auto;
+    margin-bottom: ${spacing(3)};
+    max-width: 600px;
+    width: 100%;
+    img {
+        display: block;
+        width: 100%;
+    }
+`
+export default Logo
---
+++ results/surveys/td2025/theme/typography.ts
@@ -0,0 +1,36 @@
+import { DefaultTheme } from 'styled-components'
+
+const fontFamily = `Inter var, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI,
+Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji,
+Segoe UI Symbol, Noto Color Emoji`
+
+const stateOfCSSThemeTypography: DefaultTheme['typography'] = {
+    fontFamily,
+    fontFamily2: fontFamily,
+    rootSize: {
+        mobile: '13px',
+        desktop: '15px'
+    },
+    size: {
+        smallest: '0.5rem',
+        smaller: '0.75rem',
+        small: '0.8rem',
+        smallish: '0.9rem',
+        medium: '1rem',
+        large: '1.1rem',
+        larger: '1.3rem',
+        largerer: '1.6rem',
+        largest: '2rem',
+        largerest: '2.5rem',
+        huge: '4rem',
+        huger: '5rem',
+        hugest: '6rem'
+    },
+    weight: {
+        light: 300,
+        medium: 400,
+        bold: 700
+    }
+}
+
+export default stateOfCSSThemeTypography
