b0912a0eccd9201b793915229c73f9cbf281c98b
Sacha Greif <github@sachagreif.com>
2025-12-02 11:08:38 +0900

    📊 add multi item experience item sections when appropriate

--- results/src/core/charts/horizontalBar2/rows/RowHeading.tsx
+++ results/src/core/charts/horizontalBar2/rows/RowHeading.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ChartItem } from '../../common2/item'
 import { RowComponentProps } from '../types'
 import { FreeformIndicator } from 'core/charts/common2'
+import T from 'core/i18n/T'
 
 export const RowHeading = ({
     bucket,
@@ -15,18 +16,25 @@ export const RowHeading = ({
     const { isFreeformData } = bucket
     const i18nNamespace =
         block?.filtersState?.axis1?.id || block.i18nNamespace || block.fieldId || block.id
+    const sectionId = bucket?._metadata?.sectionId
     return (
         <h3 className="chart-row-heading">
-            <ChartItem
-                id={id}
-                entity={entity}
-                bucket={bucket}
-                i18nNamespace={i18nNamespace}
-                label={label}
-                serieMetadata={serieMetadata}
-            />
-
-            {isFreeformData && <FreeformIndicator />}
+            {sectionId && (
+                <span className="chart-row-heading-section">
+                    <T k={`${i18nNamespace}.${sectionId}`} />
+                </span>
+            )}
+            <div className="chart-row-heading-content">
+                <ChartItem
+                    id={id}
+                    entity={entity}
+                    bucket={bucket}
+                    i18nNamespace={i18nNamespace}
+                    label={label}
+                    serieMetadata={serieMetadata}
+                />
+                {isFreeformData && <FreeformIndicator />}
+            </div>
         </h3>
     )
 }
