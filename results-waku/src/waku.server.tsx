62dedc27730683ba379cd6305b0c6b3127b067ef
araya <araya@araya.dev>
2026-03-16 01:46:50 +0900

    Simplify loading sitemaps
    Signed-off-by: araya <araya@araya.dev>

--- results-waku/src/waku.server.tsx
+++ results-waku/src/waku.server.tsx
@@ -3,8 +3,18 @@ import { createPages } from 'waku'
 import { RootLayout } from './templates/root-layout'
 import { LocalizedPage } from './templates/localized-page'
 import { HomePage } from './templates/home-page'
+import { loadSitemap } from './load-sitemap'
 
 // export default adapter(handler)
+
+const sitemaps = loadSitemap()
+
+const paths: Array<[locale: string, slug: string]> = sitemaps.map(page => [
+    'en-US',
+    page.path.replace(/^\/|\/$/g, '') || ''
+])
+
+console.log('sitemap paths:', paths)
 
 const pages = createPages(async ({ createPage, createRoot }) => [
     createRoot({
@@ -22,7 +32,7 @@ const pages = createPages(async ({ createPage, createRoot }) => [
         render: 'static',
         path: '/[locale]/[slug]',
         component: LocalizedPage,
-        staticPaths: [['en-US', 'features']]
+        staticPaths: paths
     })
 ])
 
