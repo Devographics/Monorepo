17a277d882f1fdd3226d8724c51ef6c1819e3620
Sacha Greif <github@sachagreif.com>
2026-08-26 11:34:46 +0900

    ✍️ add post-survey iframe newsletter signup

--- surveyform/src/components/share/TokyoDevNewsletterSignup.tsx
+++ surveyform/src/components/share/TokyoDevNewsletterSignup.tsx
@@ -1,15 +1,37 @@
 "use client";
+import { useEffect, useRef, useState } from "react";
 import { T } from "@devographics/react-i18n";
 
 const TokyoDevNewsletterSignup = () => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const [height, setHeight] = useState(600);
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.source !== iframeRef.current?.contentWindow) return;
+      const newHeight = event.data?.tokyodevMailingListWidgetHeight;
+      if (newHeight) setHeight(newHeight);
+    };
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
+
   return (
     <div className="tokyodev-newsletter">
-      <h4>
+      {/* <h4>
         <T token="finish.tokyodev_newsletter.heading" />
       </h4>
       <div className="tokyodev-newsletter-content">
         <T token="finish.tokyodev_newsletter.description" />
-      </div>
+      </div> */}
+      <iframe
+        ref={iframeRef}
+        src="https://www.tokyodev.com/survey_newsletter"
+        title="Subscribe to the TokyoDev newsletter"
+        loading="lazy"
+        scrolling="no"
+        style={{ display: "block", width: "100%", border: 0, height }}
+      />
     </div>
   );
 };
