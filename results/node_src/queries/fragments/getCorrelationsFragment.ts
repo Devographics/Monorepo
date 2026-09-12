e6247dd558e8a1d98dbf5767ad9241c363145c18
Sacha Greif <github@sachagreif.com>
2026-09-12 15:57:40 +0900

    📊 fix correlation fragment indentation

--- results/node_src/queries/fragments/getCorrelationsFragment.ts
+++ results/node_src/queries/fragments/getCorrelationsFragment.ts
@@ -1,5 +1,22 @@
 export const getCorrelationsFragment = () => `_correlations {
     questionCorrelations {
+        kind1
+        kind2
+        questionId1
+        sectionId1
+        optionId1
+        questionId2
+        sectionId2
+        optionId2
+        n
+        correlation
+        sameSection
+        strength
+        direction
+    }
+    optionCorrelations {
+        id
+        correlations {
             kind1
             kind2
             questionId1
@@ -14,23 +31,6 @@ export const getCorrelationsFragment = () => `_correlations {
             strength
             direction
         }
-        optionCorrelations {
-            id
-            correlations {
-                kind1
-                kind2
-                questionId1
-                sectionId1
-                optionId1
-                questionId2
-                sectionId2
-                optionId2
-                n
-                correlation
-                sameSection
-                strength
-                direction
-            }
-        }
     }
+}
 `
